0858d3e6fa68d1b2eebfbe441b8d14f4d10249da
Slobodan <sloooba@yahoo.com>
2023-01-26 11:11:04 +0100

    Remove the 'isDark:false from the cover block'

--- patterns/hero-3.php
+++ patterns/hero-3.php
@@ -6,7 +6,7 @@
  * Categories: icecubo-hero
  */
 ?>
-<!-- wp:cover {"customGradient":"linear-gradient(180deg,rgb(0,0,0) 0%,rgb(175,211,242) 100%)","contentPosition":"top center","isDark":true,"align":"full"} -->
+<!-- wp:cover {"customGradient":"linear-gradient(180deg,rgb(0,0,0) 0%,rgb(175,211,242) 100%)","contentPosition":"top center","align":"full"} -->
 <div class="wp-block-cover alignfull is-light has-custom-content-position is-position-top-center"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim has-background-gradient" style="background:linear-gradient(180deg,rgb(0,0,0) 0%,rgb(175,211,242) 100%)"></span><div class="wp-block-cover__inner-container"><!-- wp:spacer {"height":"10px"} -->
 <div style="height:10px" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
