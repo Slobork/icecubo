cea5d4901d6b3497684d3d1fceae131cf0aaf6fd
Slobodan <sloooba@yahoo.com>
2023-01-26 21:20:11 +0100

    Add footer patterns

---
+++ patterns/footer-basic-with-title.php
@@ -0,0 +1,21 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Footer basic with site title.
+ * Slug: icecubo/footer-basic-with-title
+ * Categories: icecubo-footer
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"30px","bottom":"30px"}},"elements":{"link":{"color":{"text":"var:preset|color|gray-friend-light"}}}},"backgroundColor":"darko","textColor":"gray-light","layout":{"type":"constrained"},"fontSize":"extra-small"} -->
+<div class="wp-block-group alignfull has-gray-light-color has-darko-background-color has-text-color has-background has-link-color has-extra-small-font-size" style="padding-top:30px;padding-bottom:30px"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide"><!-- wp:site-title {"level":0} /-->
+
+<!-- wp:paragraph -->
+<p><a href="#">Privacy Policy</a> · <a href="#">Terms of Service</a> · <a href="#">Contact Us</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>© All rights reserved</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ patterns/footer-basic.php
@@ -0,0 +1,13 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Footer basic.
+ * Slug: icecubo/footer-basic
+ * Categories: icecubo-footer
+ */
+?>
+<!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|gray-friend-light"}}},"spacing":{"padding":{"top":"55px","bottom":"30px"}}},"backgroundColor":"darko","textColor":"gray-light","layout":{"inherit":true,"type":"constrained"},"fontSize":"extra-small"} -->
+<div class="wp-block-group has-gray-light-color has-darko-background-color has-text-color has-background has-link-color has-extra-small-font-size" style="padding-top:55px;padding-bottom:30px"><!-- wp:paragraph {"align":"center","fontSize":"xsmall"} -->
+<p class="has-text-align-center has-xsmall-font-size"><a href="#">Privacy Policy</a> · <a href="#">Terms of Service</a> · <a href="#">Contact Us</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
