e2187895f0dcdb41952f6f97a7d01f48b845e9a8
Slobodan <sloooba@yahoo.com>
2023-01-31 11:09:15 +0100

    Add General category

--- inc/register-block-patterns.php
+++ inc/register-block-patterns.php
@@ -19,6 +19,11 @@ if (! function_exists('icecubo_register_block_pattern_category') ) {
     function icecubo_register_block_pattern_category() {
 
         register_block_pattern_category(
+            'icecubo-general',
+            array( 'label' => esc_html__('IceCubo - General', 'icecubo') )
+        );
+
+        register_block_pattern_category(
             'icecubo-footer',
             array( 'label' => esc_html__('IceCubo - Footer', 'icecubo') )
         );
