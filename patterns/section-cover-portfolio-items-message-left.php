c450ec8b6b7f5065c9e41040b73538fbd48d4909
Slobodan <sloooba@yahoo.com>
2023-02-01 10:53:51 +0100

    Add opacity to the cover block and change the title of the pattern

--- patterns/section-cover-portfolio-items-message-left.php
+++ patterns/section-cover-portfolio-items-message-left.php
@@ -1,13 +1,13 @@
 <?php
 // phpcs:ignore
 /**
- * Title: Portfolio items with message on left.
+ * Title: Radial gradient and opacity applied (portfolio as placeholder).
  * Slug: icecubo/section-cover-porfolio-items-radial-gradient
  * Categories: icecubo-section-covers
  */
 ?>
-<!-- wp:cover {"gradient":"darko-to-primary-radial-gradual","isDark":false,"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|80","bottom":"var:preset|spacing|60","left":"var:preset|spacing|80"}}}} -->
-<div class="wp-block-cover is-light" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--80)"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim has-background-gradient has-darko-to-primary-radial-gradual-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:columns {"verticalAlignment":"center","align":"wide"} -->
+<!-- wp:cover {"dimRatio":60,"gradient":"darko-to-primary-radial-gradual","isDark":false,"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|80","bottom":"var:preset|spacing|60","left":"var:preset|spacing|80"}}}} -->
+<div class="wp-block-cover is-light" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--80)"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-60 has-background-dim has-background-gradient has-darko-to-primary-radial-gradual-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:columns {"verticalAlignment":"center","align":"wide"} -->
 <div class="wp-block-columns alignwide are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"40%"} -->
 <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:40%"><!-- wp:heading -->
 <h2>Projects</h2>
