f1747ac69ff12e2316df3029bc828ab309207d06
Slobodan <sloooba@yahoo.com>
2023-02-01 16:53:38 +0100

    Change output

---
+++ patterns/hero-three-columns-cta.php
@@ -0,0 +1,51 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Three columns with images and buttons as placeholders.
+ * Slug: icecubo/hero-three-columns-img-btn-cta
+ * Categories: icecubo-hero
+ */
+?>
+<!-- wp:cover {"gradient":"darko-to-primary-linear-gradual","contentPosition":"top center","isDark":false,"align":"full","className":"is-light"} -->
+<div class="wp-block-cover alignfull is-light has-custom-content-position is-position-top-center"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim has-background-gradient has-darko-to-primary-linear-gradual-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"textAlign":"center","textColor":"highlight-2"} -->
+<h2 class="has-text-align-center has-highlight-2-color has-text-color"></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","textColor":"white","fontSize":"large"} -->
+<p class="has-text-align-center has-white-color has-text-color has-large-font-size">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"60px"} -->
+<div style="height:60px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph {"align":"center","textColor":"white"} -->
+<p class="has-text-align-center has-white-color has-text-color">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button /-->
+
+<!-- wp:button /--></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div></div>
+<!-- /wp:cover -->
---
+++ patterns/hero-two-columns-cta-above-fullwidth-img.php
@@ -0,0 +1,71 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Two columns CTA above full width image as placeholders.
+ * Slug: icecubo/two-columns-cta-above-fullwidth-img
+ * Categories: icecubo-hero
+ */
+?>
+<!-- wp:cover {"gradient":"darko-to-primary-linear-gradual","contentPosition":"center center","isDark":false,"align":"full","className":"is-light"} -->
+<div class="wp-block-cover alignfull is-light"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim has-background-gradient has-darko-to-primary-linear-gradual-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:spacer {"height":"70px"} -->
+<div style="height:70px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:columns {"verticalAlignment":"center"} -->
+<div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"65%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:65%"><!-- wp:heading {"textColor":"highlight-2"} -->
+<h2 class="has-highlight-2-color has-text-color"></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"textColor":"highlight-3"} -->
+<p class="has-highlight-3-color has-text-color"><strong>Lorem ipsum dolor sit amet</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button /-->
+
+<!-- wp:button /--></div>
+<!-- /wp:buttons -->
+
+<!-- wp:paragraph {"textColor":"white","fontSize":"extra-small"} -->
+<p class="has-white-color has-text-color has-extra-small-font-size">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"35%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:35%"><!-- wp:paragraph {"textColor":"white"} -->
+<p class="has-white-color has-text-color"><strong>Nunc egestas</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"5px"}}},"textColor":"white"} -->
+<p class="has-white-color has-text-color" style="margin-top:5px">Nullam tincidunt adipiscing enim. Morbi nec metus.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"textColor":"white"} -->
+<p class="has-white-color has-text-color"><strong>Nunc egestas</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"5px"}}},"textColor":"white"} -->
+<p class="has-white-color has-text-color" style="margin-top:5px">Nullam tincidunt adipiscing enim. Morbi nec metus.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"textColor":"white"} -->
+<p class="has-white-color has-text-color"><strong>Nunc egestas</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"5px"}}},"textColor":"white"} -->
+<p class="has-white-color has-text-color" style="margin-top:5px">Nullam tincidunt adipiscing enim. Morbi nec metus.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"80px"} -->
+<div style="height:80px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image --></div></div>
+<!-- /wp:cover -->
