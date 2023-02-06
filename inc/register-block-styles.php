0d2950c2cf8de1ac5004ee6bcf69d01608e2fb8a
Slobodan <sloooba@yahoo.com>
2023-02-06 17:13:38 +0100

    Correct the indentation

--- inc/register-block-styles.php
+++ inc/register-block-styles.php
@@ -17,38 +17,38 @@ if (! function_exists('icecubo_register_block_styles') ) {
     // phpcs:ignore
     function icecubo_register_block_styles()
     {
-        
+
         register_block_style(
             'core/button',
             array(
                 'name'  => 'icecubo-shadow-button',
                 'label' => __('Shadow', 'icecubo'),
-                )
-            );
-            
-            register_block_style(
-                'core/button',
+            )
+        );
+
+        register_block_style(
+            'core/button',
             array(
                 'name'  => 'icecubo-noborder-button',
                 'label' => __('No border', 'icecubo'),
-                )
-            );
+            )
+        );
 
-            register_block_style(
-                'core/heading',
-                array(
-                    'name'  => 'icecubo-text-shadow-heading',
-                    'label' => __('Shadow', 'icecubo'),
-                )
-            );
+        register_block_style(
+            'core/heading',
+            array(
+                'name'  => 'icecubo-text-shadow-heading',
+                'label' => __('Shadow', 'icecubo'),
+            )
+        );
 
-            register_block_style(
-                'core/list',
-                array(
-                    'name'  => 'icecubo-small-margin-list',
-                    'label' => __('Small margin', 'icecubo'),
-                )
-            );
+        register_block_style(
+            'core/list',
+            array(
+                'name'  => 'icecubo-small-margin-list',
+                'label' => __('Small margin', 'icecubo'),
+            )
+        );
 
         register_block_style(
             'core/paragraph',
