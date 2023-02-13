a8df419b7ad0e898c5a0cd3be2654aff54a1b474
Slobodan <sloooba@yahoo.com>
2023-02-13 22:00:08 +0100

    Add icecubo_start function

--- inc/key.php
+++ inc/key.php
@@ -7,6 +7,45 @@
  */
 
 
+if (! function_exists('icecubo_start') ) {
+
+    /**
+     * Theme basic support.
+     *
+     * @return void.
+     */
+    // phpcs:ignore
+    function icecubo_start()
+    {
+        /**
+         * Theme supprt.
+         * 
+         * These features are enabled by default in block themes from version 5.9:
+         * 'post-thumbnails'
+         * 'responsive-embeds'
+         * 'editor-styles'
+         * 'html5'
+         * 'automatic-feed-links'
+         * 
+         * @see https://developer.wordpress.org/reference/functions/add_theme_support/#comment-5596
+         */
+        add_theme_support('wp-block-styles');
+        
+        // https://developer.wordpress.org/block-editor/how-to-guides/themes/theme-support/
+        add_theme_support('custom-spacing');
+        add_theme_support('appearance-tools');
+
+        // translation support
+        load_theme_textdomain('icecubo', get_template_directory() . '/languages');
+
+         // enqueue
+         add_action('wp_enqueue_scripts', 'icecubo_scripts_and_styles');
+
+    }
+    add_action('after_setup_theme', 'icecubo_start');
+}
+
+
 if (! function_exists('icecubo_scripts_and_styles') ) {
 
     /**
@@ -23,11 +62,10 @@ if (! function_exists('icecubo_scripts_and_styles') ) {
                 'icecubo-main',
                 get_template_directory_uri() . '/assets/css/main.css',
                 array(),
-                '0.0.1',
+                ICECUBO_VERSION,
                 'all'
             );
         }
 
     }
-    add_action('wp_enqueue_scripts', 'icecubo_scripts_and_styles');
 }
