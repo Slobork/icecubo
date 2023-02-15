a0bd362802d52d6f4df0facd7daaf50b1c3c2729
Slobodan <sloooba@yahoo.com>
2023-02-15 11:55:27 +0100

    Add file

---
+++ patterns/hero-media-text-cta-left-aligned.php
@@ -0,0 +1,44 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Media and text as a call to action with heading and left aligned.
+ * Slug: icecubo/hero-media-text-cta-left-aligned
+ * Categories: icecubo-hero
+ */
+?>
+<!-- wp:cover {"gradient":"darko-to-primary-linear-gradual","contentPosition":"top center","isDark":false,"align":"full","className":"is-light"} -->
+<div class="wp-block-cover alignfull is-light has-custom-content-position is-position-top-center"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim has-background-gradient has-darko-to-primary-linear-gradual-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"textAlign":"center","textColor":"highlight-2"} -->
+<h2 class="has-text-align-center has-highlight-2-color has-text-color"></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"padding":{"left":"var:preset|spacing|80","right":"var:preset|spacing|80"}}},"textColor":"white","fontSize":"large"} -->
+<p class="has-text-align-center has-white-color has-text-color has-large-font-size" style="padding-right:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--80)">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:media-text {"align":"","mediaPosition":"right","mediaType":"image","mediaSizeSlug":"large","textColor":"white"} -->
+<div class="wp-block-media-text has-media-on-the-right is-stacked-on-mobile has-white-color has-text-color"><div class="wp-block-media-text__content">
+<!-- wp:heading {"level":3,"textColor":"highlight-3"} -->
+<h3 class="has-highlight-3-color has-text-color"></h3>
+<!-- /wp:heading -->
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+<!-- /wp:paragraph -->
+<!-- wp:buttons {"layout":{"type":"flex"}} -->
+<div class="wp-block-buttons"><!-- wp:button /--></div>
+<!-- /wp:buttons --></div><figure class="wp-block-media-text__media"><img src="" alt=""/></figure></div>
+<!-- /wp:media-text --></div>
+
+<!-- wp:spacer {"height":"30px"} -->
+<div style="height:30px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+</div>
+<!-- /wp:cover -->
