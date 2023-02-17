baf623c9d87e9051c278a5fea0c92c6d492261e2
Slobodan <sloooba@yahoo.com>
2023-02-17 13:38:05 +0100

    Add file

---
+++ patterns/header-cover-gradient-plus-logo-menu-hero.php
@@ -0,0 +1,56 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Cover with gradient with menu and hero sections.
+ * Slug: icecubo/header-cover-gradient-plus-logo-menu-hero
+ * Categories: icecubo-header
+ */
+?>
+<!-- wp:cover {"minHeight":300,"gradient":"darko-to-primary-linear-gradual","contentPosition":"center center","isDark":false,"align":"full","style":{"spacing":{"padding":{"bottom":"5rem"}}}} -->
+<div class="wp-block-cover alignfull is-light" style="padding-bottom:5rem;min-height:300px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim has-background-gradient has-darko-to-primary-linear-gradual-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"padding":{"top":"10px","bottom":"10px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:10px;padding-bottom:10px"><!-- wp:group {"layout":{"type":"flex","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex"}} -->
+<div class="wp-block-group"><!-- wp:site-logo {"shouldSyncIcon":false} /-->
+
+<!-- wp:group -->
+<div class="wp-block-group"><!-- wp:site-title {"level":0,"fontSize":"xl"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:navigation {"textColor":"white-ice","overlayBackgroundColor":"darko-transit","className":"is-style-icecubo-nav-mark-hovers","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right","orientation":"horizontal","flexWrap":"nowrap"},"style":{"spacing":{"margin":{"top":"0"}}}} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"textAlign":"center","level":1,"textColor":"highlight-2","className":"is-style-icecubo-gray-text-shadow-heading"} -->
+<h1 class="has-text-align-center is-style-icecubo-gray-text-shadow-heading has-highlight-2-color has-text-color">IceCubo WordPress Theme</h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","textColor":"white","fontSize":"large"} -->
+<p class="has-text-align-center has-white-color has-text-color has-large-font-size">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:media-text {"className":"has-media-on-the-right has-white-color has-text-color"} -->
+<div class="wp-block-media-text alignwide is-stacked-on-mobile has-media-on-the-right has-white-color has-text-color"><figure class="wp-block-media-text__media"></figure><div class="wp-block-media-text__content"><!-- wp:heading {"textColor":"highlight-3","className":"is-style-icecubo-gray-text-shadow-heading"} -->
+<h2 class="is-style-icecubo-gray-text-shadow-heading has-highlight-3-color has-text-color">It's a Block Time!</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>It's the right time to start building Websites with blocks and predefined patterns.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Start Now!</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div></div>
+<!-- /wp:media-text -->
+</div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
