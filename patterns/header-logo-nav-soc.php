ade01a1763410bbd261a95b8fdfdc30d16d020ef
Slobodan <sloooba@yahoo.com>
2023-02-20 17:05:51 +0100

    Remove size class

--- patterns/header-logo-nav-soc.php
+++ patterns/header-logo-nav-soc.php
@@ -6,8 +6,8 @@
  * Categories: icecubo-header
  */
 ?>
-<!-- wp:group {"align":"full","className":"is-style-default has-extra-small-font-size","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull is-style-default has-extra-small-font-size"><!-- wp:group {"style":{"spacing":{"padding":{"top":"25px","bottom":"25px"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<!-- wp:group {"align":"full","className":"is-style-default","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-style-default"><!-- wp:group {"style":{"spacing":{"padding":{"top":"25px","bottom":"25px"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group" style="padding-top:25px;padding-bottom:25px"><!-- wp:site-logo {"className":"is-style-default"} /-->
 
 <!-- wp:navigation {"layout":{"type":"flex","justifyContent":"center"}} /-->
