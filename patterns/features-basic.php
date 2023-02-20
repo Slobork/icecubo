59c048751037241ddcb77d8a4d7ca1a35ff2c983
Slobodan <sloooba@yahoo.com>
2023-02-20 17:00:03 +0100

    Typo

---
+++ patterns/features-basic.php
@@ -0,0 +1,39 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Features basic.
+ * Slug: icecubo/feature-basic
+ * Categories: icecubo-general
+ */
+?>
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph -->
+<p><strong>Nunc egestas</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Nullam tincidunt adipiscing enim. Morbi nec metus.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph -->
+<p><strong>Nunc egestas</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Nullam tincidunt adipiscing enim. Morbi nec metus.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph -->
+<p><strong>Nunc egestas</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Nullam tincidunt adipiscing enim. Morbi nec metus.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
