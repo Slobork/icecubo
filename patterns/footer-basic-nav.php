578c3a9356b49bf1ffd2a05a6912a7ece327d016
Slobodan <sloooba@yahoo.com>
2023-02-28 13:10:53 +0100

    Add file

---
+++ patterns/footer-basic-nav.php
@@ -0,0 +1,13 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Basic with nav menu.
+ * Slug: icecubo/footer-basic-nav
+ * Categories: icecubo-footer
+ */
+?>
+<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|tint-3"}}},"spacing":{"padding":{"top":"3em","bottom":"2em"}}},"backgroundColor":"darko","textColor":"gray-light","layout":{"inherit":true,"type":"constrained"},"fontSize":"small"} -->
+<div class="wp-block-group alignfull has-gray-light-color has-darko-background-color has-text-color has-background has-link-color has-small-font-size" style="padding-top:3em;padding-bottom:2em">
+    <!-- wp:navigation {"layout":{"type":"flex","justifyContent":"center","orientation":"horizontal","flexWrap":"wrap"},"style":{"spacing":{"margin":{"top":"0"}}}} /-->
+</div>
+<!-- /wp:group -->
