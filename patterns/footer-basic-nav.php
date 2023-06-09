46959a066d062ab948cf67d90f9c4f42a89adb99
Slobodan <sloooba@yahoo.com>
2023-06-09 18:06:05 +0200

    Nav: disalbe overlay menu even on mobile

--- patterns/footer-basic-nav.php
+++ patterns/footer-basic-nav.php
@@ -8,6 +8,6 @@
 ?>
 <!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|tint-3"}}},"spacing":{"padding":{"top":"3em","bottom":"2em"}}},"backgroundColor":"darko","textColor":"gray-light","layout":{"inherit":true,"type":"constrained"},"fontSize":"small"} -->
 <div class="wp-block-group alignfull has-gray-light-color has-darko-background-color has-text-color has-background has-link-color has-small-font-size" style="padding-top:3em;padding-bottom:2em">
-    <!-- wp:navigation {"layout":{"type":"flex","justifyContent":"center","orientation":"horizontal","flexWrap":"wrap"},"style":{"spacing":{"margin":{"top":"0"}}}} /-->
+    <!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","justifyContent":"center","orientation":"horizontal","flexWrap":"wrap"},"style":{"spacing":{"margin":{"top":"0"}}}} /-->
 </div>
 <!-- /wp:group -->
