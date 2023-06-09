79d615d7da41839fd6dfb5edebf99778286707a9
Slobodan <sloooba@yahoo.com>
2023-06-09 12:57:22 +0200

    Add file

---
+++ patterns/footer-title-nav-soc.php
@@ -0,0 +1,29 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Site title - nav - social icons.
+ * Slug: icecubo/footer-logo-nav-soc
+ * Categories: icecubo-footer
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"3em","bottom":"2em"}},"elements":{"link":{"color":{"text":"var:preset|color|tint-3"}}}},"backgroundColor":"darko","textColor":"gray-light","className":"is-style-default","layout":{"type":"constrained"},"fontSize":"small"} -->
+<div class="wp-block-group alignfull is-style-default has-gray-light-color has-darko-background-color has-text-color has-background has-link-color has-small-font-size" style="padding-top:3em;padding-bottom:2em"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"left","orientation":"horizontal"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>©</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:site-title {"level":0} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","justifyContent":"space-between","orientation":"horizontal","flexWrap":"wrap"},"style":{"spacing":{"margin":{"top":"0"}}}} /-->
+
+<!-- wp:social-links {"className":"has-icon-color has-icon-background-color is-style-default","layout":{"type":"flex","justifyContent":"center"}} -->
+<ul class="wp-block-social-links has-icon-color has-icon-background-color is-style-default"><!-- wp:social-link {"url":"#","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"#","service":"twitter"} /-->
+
+<!-- wp:social-link {"url":"#","service":"linkedin"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
