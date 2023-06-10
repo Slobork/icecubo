d3c2eb2edf1bcd3d9337ff29c49abe258f6d020d
Slobodan <sloooba@yahoo.com>
2023-06-10 10:51:36 +0200

    Add padding at Cover's bottom

--- patterns/section-cover-portfolio-item-gallery.php
+++ patterns/section-cover-portfolio-item-gallery.php
@@ -6,12 +6,12 @@
  * Categories: icecubo-section-covers
  */
 ?>
-<!-- wp:cover {"overlayColor":"primary"} -->
-<div class="wp-block-cover"><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide"><!-- wp:columns {"verticalAlignment":"center","align":"wide"} -->
+<!-- wp:cover {"overlayColor":"primary","isDark":false,"style":{"spacing":{"padding":{"bottom":"3rem"}}}} -->
+<div class="wp-block-cover is-light" style="padding-bottom:3rem"><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"0","padding":{"bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="padding-bottom:0"><!-- wp:columns {"verticalAlignment":"center","align":"wide"} -->
 <div class="wp-block-columns alignwide are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center"} -->
 <div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading -->
-<h2>Sample project</h2>
+<h2 class="wp-block-heading">Sample project</h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
