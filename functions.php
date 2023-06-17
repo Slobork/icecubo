07b4d0262fb353af6fda76b8cbbf9d584bed31de
Slobodan <sloooba@yahoo.com>
2023-06-17 17:02:48 +0200

    Version bump

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@
  * Author URI:          https://maxpressy.com
  * License:             GPL v2 or later
  * License URI:         https://www.gnu.org/licenses/gpl-2.0.html
- * Version:             1.0.6
+ * Version:             1.0.7
  * Text Domain:         icecubo
  * Domain Path:         /languages
  * Requires at least:   6.1
