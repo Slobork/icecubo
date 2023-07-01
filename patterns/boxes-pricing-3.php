63ea4693534e8a17bdf99e9e3da242264aaf69b1
Slobodan <sloooba@yahoo.com>
2023-07-01 21:42:57 +0200

    add file

---
+++ patterns/boxes-pricing-3.php
@@ -0,0 +1,97 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Pricing boxes 3
+ * Slug: icecubo/boxes-pricing-3
+ * Categories: icecubo-boxes
+ */
+?>
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|large","right":"var:preset|spacing|large","bottom":"var:preset|spacing|large","left":"var:preset|spacing|large"}}},"backgroundColor":"white-ice","className":"with-text-alignment"} -->
+<div class="wp-block-column with-text-alignment has-white-ice-background-color has-background" style="padding-top:var(--wp--preset--spacing--large);padding-right:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--large);padding-left:var(--wp--preset--spacing--large)"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","right":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|x-small"}},"border":{"radius":"5px","width":"0px","style":"none"}},"backgroundColor":"highlight-2","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-highlight-2-background-color has-background" style="border-style:none;border-width:0px;border-radius:5px;padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--x-small);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--x-small)"><!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
+<p style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px"><strong>$39</strong> <sub>per month</sub></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"fontSize":"xl"} -->
+<p class="has-xl-font-size" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px">Basic</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"fontSize":"small"} -->
+<p class="has-small-font-size" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px">When you need to start smoothly with essential features</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:list -->
+<ul><!-- wp:list-item -->
+<li>Feature item #</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Feature item #</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Feature item #</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Feature item #</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Feature item #</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"},"style":{"spacing":{"margin":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|x-large"}}}} -->
+<div class="wp-block-buttons" style="margin-top:var(--wp--preset--spacing--large);margin-bottom:var(--wp--preset--spacing--x-large)"><!-- wp:button {"width":100,"style":{"border":{"radius":"6px"}}} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link wp-element-button" style="border-radius:6px">Buy now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|large","right":"var:preset|spacing|large","bottom":"var:preset|spacing|large","left":"var:preset|spacing|large"}}},"backgroundColor":"white-ice","className":"with-text-alignment"} -->
+<div class="wp-block-column with-text-alignment has-white-ice-background-color has-background" style="padding-top:var(--wp--preset--spacing--large);padding-right:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--large);padding-left:var(--wp--preset--spacing--large)"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","right":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|x-small"}},"border":{"radius":"5px","width":"0px","style":"none"}},"backgroundColor":"highlight-2","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-highlight-2-background-color has-background" style="border-style:none;border-width:0px;border-radius:5px;padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--x-small);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--x-small)"><!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
+<p style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px"><strong>$69</strong> <sub>per month</sub></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"fontSize":"xl"} -->
+<p class="has-xl-font-size" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px">Professional</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"fontSize":"small"} -->
+<p class="has-small-font-size" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px">Great for those who need the professional features</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:list -->
+<ul><!-- wp:list-item -->
+<li>Feature item #</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Feature item #</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Feature item #</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Feature item #</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Feature item #</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"},"style":{"spacing":{"margin":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|x-large"}}}} -->
+<div class="wp-block-buttons" style="margin-top:var(--wp--preset--spacing--large);margin-bottom:var(--wp--preset--spacing--x-large)"><!-- wp:button {"width":100,"style":{"border":{"radius":"6px"}}} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link wp-element-button" style="border-radius:6px">Buy now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
---
+++ patterns/boxes-pricing-4.php
@@ -0,0 +1,137 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Pricing boxes 4
+ * Slug: icecubo/boxes-pricing-4
+ * Categories: icecubo-boxes
+ */
+?>
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"backgroundColor":"white-ice","className":"with-text-alignment"} -->
+<div class="wp-block-column with-text-alignment has-white-ice-background-color has-background" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","right":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|x-small"},"margin":{"bottom":"var:preset|spacing|x-large"}}},"backgroundColor":"highlight-2","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-highlight-2-background-color has-background" style="margin-bottom:var(--wp--preset--spacing--x-large);padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--x-small);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--x-small)"><!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
+<p style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px"><strong>$39</strong> <sub>per month</sub></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"fontSize":"xl"} -->
+<p class="has-xl-font-size" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px">Basic</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"fontSize":"small"} -->
+<p class="has-small-font-size" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px">When you need to start smoothly with essential features</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"1rem"}}},"fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size" style="margin-top:1rem">Feature item #</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:separator {"style":{"spacing":{"margin":{"top":"5px","bottom":"5px"}}},"backgroundColor":"gray-dark","className":"is-style-wide"} -->
+<hr class="wp-block-separator has-text-color has-gray-dark-color has-alpha-channel-opacity has-gray-dark-background-color has-background is-style-wide" style="margin-top:5px;margin-bottom:5px"/>
+<!-- /wp:separator -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"1rem"}}},"fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size" style="margin-top:1rem">Feature item #</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:separator {"style":{"spacing":{"margin":{"top":"5px","bottom":"5px"}}},"backgroundColor":"gray-dark","className":"is-style-wide"} -->
+<hr class="wp-block-separator has-text-color has-gray-dark-color has-alpha-channel-opacity has-gray-dark-background-color has-background is-style-wide" style="margin-top:5px;margin-bottom:5px"/>
+<!-- /wp:separator -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"1rem"}}},"fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size" style="margin-top:1rem">Feature item #</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:separator {"style":{"spacing":{"margin":{"top":"5px","bottom":"5px"}}},"backgroundColor":"gray-dark","className":"is-style-wide"} -->
+<hr class="wp-block-separator has-text-color has-gray-dark-color has-alpha-channel-opacity has-gray-dark-background-color has-background is-style-wide" style="margin-top:5px;margin-bottom:5px"/>
+<!-- /wp:separator -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"1rem"}}},"fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size" style="margin-top:1rem">Feature item #</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:separator {"style":{"spacing":{"margin":{"top":"5px","bottom":"5px"}}},"backgroundColor":"gray-dark","className":"is-style-wide"} -->
+<hr class="wp-block-separator has-text-color has-gray-dark-color has-alpha-channel-opacity has-gray-dark-background-color has-background is-style-wide" style="margin-top:5px;margin-bottom:5px"/>
+<!-- /wp:separator -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"1rem"}}},"fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size" style="margin-top:1rem">Feature item #</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:separator {"style":{"spacing":{"margin":{"top":"5px","bottom":"5px"}}},"backgroundColor":"gray-dark","className":"is-style-wide"} -->
+<hr class="wp-block-separator has-text-color has-gray-dark-color has-alpha-channel-opacity has-gray-dark-background-color has-background is-style-wide" style="margin-top:5px;margin-bottom:5px"/>
+<!-- /wp:separator -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"},"style":{"spacing":{"margin":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|x-large"}}}} -->
+<div class="wp-block-buttons" style="margin-top:var(--wp--preset--spacing--large);margin-bottom:var(--wp--preset--spacing--x-large)"><!-- wp:button {"width":100} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link wp-element-button">Buy now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"backgroundColor":"white-ice","className":"with-text-alignment"} -->
+<div class="wp-block-column with-text-alignment has-white-ice-background-color has-background" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","right":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|x-small"},"margin":{"bottom":"var:preset|spacing|x-large"}}},"backgroundColor":"highlight-2","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-highlight-2-background-color has-background" style="margin-bottom:var(--wp--preset--spacing--x-large);padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--x-small);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--x-small)"><!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
+<p style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px"><strong>$69</strong> <sub>per month</sub></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"fontSize":"xl"} -->
+<p class="has-xl-font-size" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px">Professional</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"fontSize":"small"} -->
+<p class="has-small-font-size" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px">Great for those who need the professional features</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"1rem"}}},"fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size" style="margin-top:1rem">Feature item #</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:separator {"style":{"spacing":{"margin":{"top":"5px","bottom":"5px"}}},"backgroundColor":"gray-dark","className":"is-style-wide"} -->
+<hr class="wp-block-separator has-text-color has-gray-dark-color has-alpha-channel-opacity has-gray-dark-background-color has-background is-style-wide" style="margin-top:5px;margin-bottom:5px"/>
+<!-- /wp:separator -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"1rem"}}},"fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size" style="margin-top:1rem">Feature item #</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:separator {"style":{"spacing":{"margin":{"top":"5px","bottom":"5px"}}},"backgroundColor":"gray-dark","className":"is-style-wide"} -->
+<hr class="wp-block-separator has-text-color has-gray-dark-color has-alpha-channel-opacity has-gray-dark-background-color has-background is-style-wide" style="margin-top:5px;margin-bottom:5px"/>
+<!-- /wp:separator -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"1rem"}}},"fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size" style="margin-top:1rem">Feature item #</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:separator {"style":{"spacing":{"margin":{"top":"5px","bottom":"5px"}}},"backgroundColor":"gray-dark","className":"is-style-wide"} -->
+<hr class="wp-block-separator has-text-color has-gray-dark-color has-alpha-channel-opacity has-gray-dark-background-color has-background is-style-wide" style="margin-top:5px;margin-bottom:5px"/>
+<!-- /wp:separator -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"1rem"}}},"fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size" style="margin-top:1rem">Feature item #</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:separator {"style":{"spacing":{"margin":{"top":"5px","bottom":"5px"}}},"backgroundColor":"gray-dark","className":"is-style-wide"} -->
+<hr class="wp-block-separator has-text-color has-gray-dark-color has-alpha-channel-opacity has-gray-dark-background-color has-background is-style-wide" style="margin-top:5px;margin-bottom:5px"/>
+<!-- /wp:separator -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"1rem"}}},"fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size" style="margin-top:1rem">Feature item #</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:separator {"style":{"spacing":{"margin":{"top":"5px","bottom":"5px"}}},"backgroundColor":"gray-dark","className":"is-style-wide"} -->
+<hr class="wp-block-separator has-text-color has-gray-dark-color has-alpha-channel-opacity has-gray-dark-background-color has-background is-style-wide" style="margin-top:5px;margin-bottom:5px"/>
+<!-- /wp:separator -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"},"style":{"spacing":{"margin":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|x-large"}}}} -->
+<div class="wp-block-buttons" style="margin-top:var(--wp--preset--spacing--large);margin-bottom:var(--wp--preset--spacing--x-large)"><!-- wp:button {"width":100} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link wp-element-button">Buy now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
