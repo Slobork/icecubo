d3eb1027eab04b349c4a6b4297044954f6d16430
Slobodan <sloooba@yahoo.com>
2023-07-03 23:14:36 +0200

    Add file

---
+++ patterns/box-cta-along-media.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Title: CTA along media
+ * Slug: icecubo/box-cta-along-media
+ * Description: 
+ * Categories: icecubo-box
+ * Keywords: 
+ * Viewport Width: 1280
+ * Block Types: 
+ * Post Types: 
+ * Inserter: true
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","right":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|medium"}}},"backgroundColor":"white-ice","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-white-ice-background-color has-background" style="padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:media-text {"mediaId":28864,"mediaLink":"http://protester.local/icecubo-blue-684x560/","mediaType":"image"} -->
+<div class="wp-block-media-text alignwide is-stacked-on-mobile"><figure class="wp-block-media-text__media"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/patterns/images/icecubo-blue-684x560-1.png" alt="" class="wp-image-28864 size-full"/></figure><div class="wp-block-media-text__content"><!-- wp:heading -->
+<h2 class="wp-block-heading"></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button /--></div>
+<!-- /wp:buttons --></div></div>
+<!-- /wp:media-text --></div>
+<!-- /wp:group -->
