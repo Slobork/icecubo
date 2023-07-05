764ef9d498b16444600ddb5199b71b0e2379f1d1
Slobodan <sloooba@yahoo.com>
2023-07-05 11:47:33 +0200

    Set the img link to the assets folder

--- patterns/box-cta-along-media.php
+++ patterns/box-cta-along-media.php
@@ -1,4 +1,5 @@
 <?php
+// phpcs:ignore
 /**
  * Title: CTA along media
  * Slug: icecubo/box-cta-along-media
@@ -12,8 +13,8 @@
  */
 ?>
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","right":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|medium"}}},"backgroundColor":"white-ice","layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-white-ice-background-color has-background" style="padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:media-text {"mediaId":28864,"mediaLink":"http://protester.local/icecubo-blue-684x560/","mediaType":"image"} -->
-<div class="wp-block-media-text alignwide is-stacked-on-mobile"><figure class="wp-block-media-text__media"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/patterns/images/icecubo-blue-684x560-1.png" alt="" class="wp-image-28864 size-full"/></figure><div class="wp-block-media-text__content"><!-- wp:heading -->
+<div class="wp-block-group has-white-ice-background-color has-background" style="padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:media-text {"mediaType":"image"} -->
+<div class="wp-block-media-text alignwide is-stacked-on-mobile"><figure class="wp-block-media-text__media"><img src="<?php echo esc_url(get_theme_file_uri()) . '/assets/img/ice-cubes.png'; ?>" alt=""/></figure><div class="wp-block-media-text__content"><!-- wp:heading -->
 <h2 class="wp-block-heading"></h2>
 <!-- /wp:heading -->
 
