060c3f2a5fd93da6a4890ab3403e6856f9f28eaa
Slobodan <sloooba@yahoo.com>
2023-08-15 21:10:47 +0200

    Adjust spacing for the wrpper group

--- patterns/post-header-basic-reverse.php
+++ patterns/post-header-basic-reverse.php
@@ -6,10 +6,10 @@
  * Categories: icecubo-post-header
  */
 ?>
-<!-- wp:group {"layout":{"type":"constrained"},"style":{"spacing":{"blockGap":".5rem"}}} -->
-<div class="wp-block-group">
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"0"},"blockGap":".25rem","padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|large"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0;padding-top:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--large)">
     <!-- wp:post-featured-image /-->
-    <!-- wp:post-title {"level":1} /-->
+    <!-- wp:post-title {"level":1,"style":{"spacing":{"margin":{"top":"var:preset|spacing|small"}}}} /-->
     <!-- wp:pattern {"slug":"icecubo/post-byline-basic"} /-->
 </div>
 <!-- /wp:group -->
