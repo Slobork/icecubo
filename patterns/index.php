c3688cc9396861ecc4d9da67ec4fe95746b97fa7
Slobodan <sloooba@yahoo.com>
2023-08-15 21:14:56 +0200

    Adjust spacing

--- patterns/index.php
+++ patterns/index.php
@@ -7,8 +7,8 @@
  * Inserter: false
  */
 ?>
-<!-- wp:group {"tagName":"main","layout":{"type":"constrained"}} -->
-<main class="wp-block-group">
+<!-- wp:group {"tagName":"main","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|x-large"},"margin":{"top":"0"}}},"layout":{"type":"constrained"}} -->
+<main class="wp-block-group" style="margin-top:0;padding-bottom:var(--wp--preset--spacing--x-large)">
     <!-- wp:group {"style":{"spacing":{"padding":{"top":"1px"}}}} -->
     <div class="wp-block-group" style="padding-top:1px">
     <!-- wp:heading {"level":1} --><h1 class="wp-block-heading"><?php esc_html_e('The latest posts', 'icecubo');?></h1><!-- /wp:heading -->
