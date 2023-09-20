a975297478a6eb6f63868e366e0965e0e79d6c62
Slobodan <sloooba@yahoo.com>
2023-09-20 20:34:16 +0200

    Change the title in desc

--- patterns/query-classic-featured-img-beneath-title.php
+++ patterns/query-classic-featured-img-beneath-title.php
@@ -1,43 +1,52 @@
 <?php
 // phpcs:ignore
 /**
- * Title: Classic query of posts (Title - byline - featured img - excerpt - terms).
+ * Title: Classic query of posts (Title first).
  * Slug: icecubo/query-classic-featured-img-beneath-title
- * Categories: hidden
- * Inserter: false
+ * Categories: icecubo-query
+ * Inserter: true
  */
 ?>
-<!-- wp:query {"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","type":"constrained","inherit":true,"perPage":1},"displayLayout":{"type":"list"},"layout":{"type":"default"}} -->
-<div class="wp-block-query">
-    <!-- wp:post-template -->
-    <!-- wp:group {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|xx-large"}}},"className":"is-style-default"} -->
-    <div class="wp-block-group is-style-default" style="margin-bottom:var(--wp--preset--spacing--xx-large)">
-        <!-- wp:post-title {"isLink":true} /--> 
-        <!-- wp:post-featured-image {"isLink":true} /-->
-        <!-- wp:pattern {"slug":"icecubo/post-byline-basic"} /-->
-        <!-- wp:post-excerpt {"showMoreOnNewLine":false} /-->
+<!-- wp:query {"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","type":"constrained","inherit":true,"perPage":6},"layout":{"type":"default"}} -->
+<div class="wp-block-query"><!-- wp:post-template {"layout":{"type":"default"}} -->
+<!-- wp:group {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|xx-large"}}},"className":"is-style-default"} -->
+<div class="wp-block-group is-style-default" style="margin-bottom:var(--wp--preset--spacing--xx-large)"><!-- wp:post-title {"isLink":true} /-->
 
-        <!-- wp:group {"layout":{"type":"flex"}, "style":{"spacing":{"blockGap":".5rem"},"fontSize":"extra-small"}} -->
-        <div class="wp-block-group has-extra-small-font-size">
-            <!-- wp:post-terms {"term":"category","className":"is-style-icecubo-oval-post-meta"} /-->
-            <!-- wp:post-terms {"term": "post_tag","className":"is-style-icecubo-oval-post-meta"} /-->
-        </div>
-        <!-- /wp:group -->
-    </div>
-    <!-- /wp:group -->
-    <!-- /wp:post-template -->
+<!-- wp:post-featured-image {"isLink":true} /-->
 
-    <!-- wp:group {"layout":{"inherit":false}} -->
-    <div class="wp-block-group">
-        <!-- wp:query-pagination {"className":"is-style-icecubo-oval-post-meta"} -->
-        <!-- wp:query-pagination-previous /-->
+<!-- wp:group {"style":{"spacing":{"blockGap":".25rem"}},"layout":{"type":"flex"},"fontSize":"extra-small"} -->
+<div class="wp-block-group has-extra-small-font-size"><!-- wp:post-date {"className":"is-style-default"} /-->
 
-        <!-- wp:query-pagination-numbers /-->
+<!-- wp:paragraph -->
+<p>·</p>
+<!-- /wp:paragraph -->
 
-        <!-- wp:query-pagination-next /-->
-        <!-- /wp:query-pagination -->
+<!-- wp:post-author {"showAvatar":false,"className":"is-style-default"} /--></div>
+<!-- /wp:group -->
 
-    </div>
-    <!-- /wp:group -->
-</div>
+<!-- wp:post-excerpt {"showMoreOnNewLine":false} /-->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":".5rem"},"fontSize":"extra-small"},"className":"has-extra-small-font-size","layout":{"type":"flex"}} -->
+<div class="wp-block-group has-extra-small-font-size"><!-- wp:post-terms {"term":"category","className":"is-style-default"} /-->
+
+<!-- wp:post-terms {"term":"post_tag","className":"is-style-default"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+<!-- /wp:post-template -->
+
+<!-- wp:group {"layout":{"inherit":false}} -->
+<div class="wp-block-group"><!-- wp:query-pagination {"className":"is-style-icecubo-oval-post-meta"} -->
+<!-- wp:query-pagination-previous /-->
+
+<!-- wp:query-pagination-numbers /-->
+
+<!-- wp:query-pagination-next /-->
+<!-- /wp:query-pagination --></div>
+<!-- /wp:group -->
+
+<!-- wp:query-no-results -->
+<!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results."} -->
+<p></p>
+<!-- /wp:paragraph -->
+<!-- /wp:query-no-results --></div>
 <!-- /wp:query -->
