38c0c05e611b496217430fbd153b3d8c9f20a9a3
Slobodan <sloooba@yahoo.com>
2023-09-22 09:19:12 +0200

    Change the php syntax for the 'The latest post' line

--- patterns/index-featured-img-aside.php
+++ patterns/index-featured-img-aside.php
@@ -11,7 +11,7 @@
 <!-- wp:group {"tagName":"main","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|x-large"},"margin":{"top":"0"}}},"layout":{"type":"constrained"}} -->
 <main class="wp-block-group" style="margin-top:0;padding-bottom:var(--wp--preset--spacing--x-large)"><!-- wp:group {"style":{"spacing":{"padding":{"top":"1px"}}}} -->
 <div class="wp-block-group" style="padding-top:1px"><!-- wp:heading {"level":1} -->
-<h1 class="wp-block-heading"><?php echo esc_html__('The latest posts', 'icecubo'); ?></h1>
+<h1 class="wp-block-heading"><?php esc_html_e('The latest posts', 'icecubo');?></h1>
 <!-- /wp:heading --></div>
 <!-- /wp:group -->
     <!-- wp:pattern {"slug":"icecubo/query-featured-img-aside"} /-->
