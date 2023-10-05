1afc33f2f2048ef240dc2493efdc4770e7f12be6
Slobodan <sloooba@yahoo.com>
2023-10-05 14:46:13 +0200

    Add the file

---
+++ patterns/post-header-fading-bg.php
@@ -0,0 +1,19 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Group with fading background.
+ * Slug: icecubo/post-header-group-fading-bg-title-byline-featured-img
+ * Categories: icecubo-post-header
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|xxx-large"},"blockGap":"var:preset|spacing|small"}},"gradient":"primary-to-transparent-rushly-radial","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-primary-to-transparent-rushly-radial-gradient-background has-background" style="padding-top:var(--wp--preset--spacing--xxx-large)"><!-- wp:post-title {"level":1,"style":{"spacing":{"margin":{"top":"var:preset|spacing|small"}}}} /-->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0.75rem"}},"layout":{"type":"flex"},"fontSize":"extra-small"} -->
+<div class="wp-block-group has-extra-small-font-size"><!-- wp:post-date {"backgroundColor":"white-ice","className":"is-style-icecubo-oval-post-meta"} /-->
+
+<!-- wp:post-author {"showAvatar":false,"backgroundColor":"white-ice","className":"is-style-icecubo-oval-post-meta"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:post-featured-image {"aspectRatio":"auto","height":"50vh"} /--></div>
+<!-- /wp:group -->
