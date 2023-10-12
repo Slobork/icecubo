18c68650b2beb994231a7cf68431efe506dd410e
Slobodan <sloooba@yahoo.com>
2023-10-12 13:35:21 +0200

    Add the file

---
+++ patterns/sidebar.php
@@ -0,0 +1,42 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Common sidebar.
+ * Slug: icecubo/sidebar
+ * Categories: hidden
+ * Inserter: false
+ */
+?>
+<!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:search {"label":"Search","showLabel":false,"placeholder":"Search...","buttonText":"Search","buttonUseIcon":true} /-->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-large","bottom":"0"}}},"className":"is-style-icecubo-mix-colors"} -->
+<p class="is-style-icecubo-mix-colors" style="margin-top:var(--wp--preset--spacing--x-large);margin-bottom:0">Latest posts:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:latest-posts {"displayPostDate":true} /-->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-large","bottom":"0"}}},"className":"is-style-icecubo-mix-colors"} -->
+<p class="is-style-icecubo-mix-colors" style="margin-top:var(--wp--preset--spacing--x-large);margin-bottom:0">Latest comments:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:latest-comments {"displayExcerpt":false} /-->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-large","bottom":"0"}}},"className":"is-style-icecubo-mix-colors"} -->
+<p class="is-style-icecubo-mix-colors" style="margin-top:var(--wp--preset--spacing--x-large);margin-bottom:0">Archives:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:archives /-->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-large","bottom":"0"}}},"className":"is-style-icecubo-mix-colors"} -->
+<p class="is-style-icecubo-mix-colors" style="margin-top:var(--wp--preset--spacing--x-large);margin-bottom:0">Let's connect:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links {"iconColor":"darko-transit","iconColorValue":"#171f29","iconBackgroundColor":"transparent","iconBackgroundColorValue":"transparent","className":"is-style-icecubo-social-outline","layout":{"type":"flex","justifyContent":"center"}} -->
+<ul class="wp-block-social-links has-icon-color has-icon-background-color is-style-icecubo-social-outline"><!-- wp:social-link {"url":"#","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"#","service":"linkedin"} /-->
+
+<!-- wp:social-link {"url":"#","service":"instagram"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group -->
