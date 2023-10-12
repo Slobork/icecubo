8490f9e9128be77f374a62b46c319b8c9f06a6f8
Slobodan <sloooba@yahoo.com>
2023-10-12 14:06:55 +0200

    Expand with more sections

--- patterns/page-about.php
+++ patterns/page-about.php
@@ -6,7 +6,7 @@
  * Categories: icecubo-page
  */
 ?>
-<!-- wp:media-text {"align":"wide","mediaType":"image","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|xxx-large"}}}} -->
+<!-- wp:media-text {"align":"wide","mediaType":"image","className":"alignwide"} -->
 <div class="wp-block-media-text alignwide is-stacked-on-mobile"><figure class="wp-block-media-text__media"><img src="<?php echo esc_url(get_theme_file_uri('/assets/img/ice-cubes.png')); ?>" alt="IceCubo placeholder image"/></figure><div class="wp-block-media-text__content"><!-- wp:heading {"level":3} -->
 <h3 class="wp-block-heading">Our Values</h3>
 <!-- /wp:heading -->
@@ -34,6 +34,26 @@
 <h2 class="wp-block-heading has-text-align-center">Lorem ipsum sit amet, consectetur a adipiscing</h2>
 <!-- /wp:heading -->
 
-<!-- wp:paragraph {"className":"has-text-align-center"} -->
-<p class="has-text-align-center"></p>
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At risus viverra adipiscing at in. Tellus mauris a diam maecenas sed enim. Vitae elementum curabitur vitae nunc sed velit dignissim. Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque. Ultricies leo integer malesuada nunc vel risus commodo viverra maecenas. Condimentum mattis pellentesque id nibh tortor. Nibh tellus molestie nunc non. Blandit massa enim nec dui nunc mattis. Sed nisi lacus sed viverra tellus.</p>
 <!-- /wp:paragraph -->
+
+<!-- wp:media-text {"align":"wide","mediaType":"image","className":"alignwide"} -->
+<div class="wp-block-media-text alignwide is-stacked-on-mobile"><figure class="wp-block-media-text__media"><img src="<?php echo esc_url(get_theme_file_uri('/assets/img/ice-cubes.png')); ?>" alt="IceCubo placeholder image"/></figure><div class="wp-block-media-text__content"><!-- wp:heading -->
+<h2 class="wp-block-heading">OUR VISION</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At risus viverra adipiscing at in. Tellus mauris a diam maecenas sed enim. Vitae elementum curabitur vitae nunc sed velit dignissim. Vitae proin sagittis snisl rhoncus mattis rhoncus urna neque. Ultricies leo integer malesuada nunc vel risus commodo viverra maecenas. Condimentum mattis pellentesque id nibh tortor. Nibh tellus molestie nunc non. Blandit massa enim nec dui nunc mattis. Sed nisi lacus sed viverra tellus.</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:media-text -->
+
+<!-- wp:media-text {"align":"wide","mediaPosition":"right","mediaType":"image","className":"alignwide"} -->
+<div class="wp-block-media-text alignwide has-media-on-the-right is-stacked-on-mobile"><div class="wp-block-media-text__content"><!-- wp:heading -->
+<h2 class="wp-block-heading">OUR MISSION</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At risus viverra adipiscing at in. Tellus mauris a diam maecenas sed enim. Vitae elementum curabitur vitae nunc sed velit dignissim. Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque. Ultricies leo integer malesuada nunc vel risus commodo viverra maecenas. Condimentum mattis pellentesque id nibh tortor. Nibh tellus molestie nunc non. Blandit massa enim nec dui nunc mattis. Sed nisi lacus sed viverra tellus.</p>
+<!-- /wp:paragraph --></div><figure class="wp-block-media-text__media"><img src="<?php echo esc_url(get_theme_file_uri('/assets/img/ice-cubes.png')); ?>" alt="IceCubo placeholder image"/></figure></div>
+<!-- /wp:media-text -->
