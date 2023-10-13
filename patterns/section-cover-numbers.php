840c05432192572649f1c1d6dc001b506c6cf682
Slobodan <sloooba@yahoo.com>
2023-10-13 19:53:02 +0200

    Add the file

---
+++ patterns/section-cover-numbers.php
@@ -0,0 +1,73 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Cover - accent numbers.
+ * Slug: icecubo/section-cover-numbers
+ * Categories: icecubo-section
+ */
+?>
+<!-- wp:cover {"dimRatio":30,"gradient":"primary-to-transparent-gradual-radial","isDark":false,"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|large","right":"var:preset|spacing|large","left":"var:preset|spacing|large"}}},"className":"is-light"} -->
+<div class="wp-block-cover alignwide is-light" style="padding-top:var(--wp--preset--spacing--large);padding-right:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--large);padding-left:var(--wp--preset--spacing--large)"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-30 has-background-dim has-background-gradient has-primary-to-transparent-gradual-radial-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:columns {"verticalAlignment":null} -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center","width":"55%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:55%"><!-- wp:heading -->
+<h2 class="wp-block-heading">Our clients are thriving with us!</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0.5rem"}}}} -->
+<p style="margin-top:0.5rem">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|large","right":"var:preset|spacing|small","bottom":"var:preset|spacing|large","left":"var:preset|spacing|small"}}}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--large);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--large);padding-left:var(--wp--preset--spacing--small)"><!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"bottom":"0"}},"typography":{"lineHeight":"1.3"}},"textColor":"primary","fontSize":"xxxl"} -->
+<p class="has-text-align-center has-primary-color has-text-color has-xxxl-font-size" style="margin-bottom:0;line-height:1.3">99%</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0"}}}} -->
+<p class="has-text-align-center" style="margin-top:0">Happy clients</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"1.5rem"} -->
+<div style="height:1.5rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"bottom":"0"}},"typography":{"lineHeight":"1.3"}},"textColor":"primary","fontSize":"xxxl"} -->
+<p class="has-text-align-center has-primary-color has-text-color has-xxxl-font-size" style="margin-bottom:0;line-height:1.3">10+</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0"}}}} -->
+<p class="has-text-align-center" style="margin-top:0">Awards</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|large","right":"var:preset|spacing|small","bottom":"var:preset|spacing|large","left":"var:preset|spacing|small"}}}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--large);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--large);padding-left:var(--wp--preset--spacing--small)"><!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"bottom":"0"}},"typography":{"lineHeight":"1.3"}},"textColor":"primary","fontSize":"xxxl"} -->
+<p class="has-text-align-center has-primary-color has-text-color has-xxxl-font-size" style="margin-bottom:0;line-height:1.3">25K</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0"}}}} -->
+<p class="has-text-align-center" style="margin-top:0">Active users</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"1.5rem"} -->
+<div style="height:1.5rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"bottom":"0"}},"typography":{"lineHeight":"1.3"}},"textColor":"primary","fontSize":"xxxl"} -->
+<p class="has-text-align-center has-primary-color has-text-color has-xxxl-font-size" style="margin-bottom:0;line-height:1.3">15yr.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0"}}}} -->
+<p class="has-text-align-center" style="margin-top:0">In Business</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div></div>
+<!-- /wp:cover -->
