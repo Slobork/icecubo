21442642b90f2e04181a50f97820c56fffca3419
Slobodan <sloooba@yahoo.com>
2023-10-17 18:12:44 +0200

    Add the file

---
+++ patterns/boxes-with-cover-img-colorized-rotate-1.php
@@ -0,0 +1,53 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Gruop boxes in columns, with cover - applyied background color + rotate 1.
+ * Slug: icecubo/boxes-with-cover-bg-colorized-rotate-1
+ * Categories: icecubo-box
+ */
+?>
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:cover {"minHeight":500,"gradient":"matching-colors-diagonal","align":"center","style":{"color":[]}} -->
+<div class="wp-block-cover aligncenter" style="min-height:500px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim has-background-gradient has-matching-colors-diagonal-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":3} -->
+<h3 class="wp-block-heading has-text-align-center"></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Lorem ipsum odor amet, consectetuer adipiscing elit.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center","orientation":"horizontal"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Learn more</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:cover {"dimRatio":60,"gradient":"matching-colors-diagonal","contentPosition":"center center","className":"is-style-icecubo-rotate-left"} -->
+<div class="wp-block-cover is-style-icecubo-rotate-left"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-60 has-background-dim has-background-gradient has-matching-colors-diagonal-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","className":"is-style-icecubo-mix-colors-focus","fontSize":"xl"} -->
+<p class="has-text-align-center is-style-icecubo-mix-colors-focus has-xl-font-size"><em>AMAZING FEATURES</em></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:cover {"minHeight":500,"gradient":"matching-colors-diagonal","align":"center","style":{"color":[]}} -->
+<div class="wp-block-cover aligncenter" style="min-height:500px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim has-background-gradient has-matching-colors-diagonal-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":3} -->
+<h3 class="wp-block-heading has-text-align-center"></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Lorem ipsum odor amet, consectetuer adipiscing elit.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center","orientation":"horizontal"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Learn more</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
