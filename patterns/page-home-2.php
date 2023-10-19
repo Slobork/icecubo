7e43260b7a0322064192936a752652321a750da2
Slobodan <sloooba@yahoo.com>
2023-10-19 19:41:23 +0200

    Add the file

---
+++ patterns/page-home-2.php
@@ -0,0 +1,30 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Home page 2.
+ * Slug: icecubo/page-home-2
+ * Categories: icecubo-page
+ * Template Types: front-page, page
+ */
+?>
+<!-- wp:pattern {"slug":"icecubo/header-cover-gradient-plus-logo-menu-hero-light"} /-->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">Lorem <mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-matching-6-color">ipsum dolor sit amet</mark>, consectetur adipiscing elit, sed do eiusmod <mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-matching-6-color">tempor</mark></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At risus viverra adipiscing at in. Tellus mauris a diam maecenas sed enim. Vitae elementum curabitur vitae nunc sed velit dignissim. Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque. </p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"border":{"bottom":{"width":"1px"},"top":{"width":"1px"}},"spacing":{"padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|large"},"margin":{"top":"var:preset|spacing|xx-large","bottom":"var:preset|spacing|xx-large"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="border-top-width:1px;border-bottom-width:1px;margin-top:var(--wp--preset--spacing--xx-large);margin-bottom:var(--wp--preset--spacing--xx-large);padding-top:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--large)">
+<!-- wp:pattern {"slug":"icecubo/features-basic"} /-->
+<!-- wp:pattern {"slug":"icecubo/features-basic"} /-->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:pattern {"slug":"icecubo/section-cover-numbers"} /-->
+<!-- wp:pattern {"slug":"icecubo/testimonials-basic"} /-->
