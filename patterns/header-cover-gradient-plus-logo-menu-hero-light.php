020384aec86da416d911fdafb123008d1c43ff95
Slobodan <sloooba@yahoo.com>
2023-10-19 12:58:22 +0200

    Change the colors for the headings and buttons

--- patterns/header-cover-gradient-plus-logo-menu-hero-light.php
+++ patterns/header-cover-gradient-plus-logo-menu-hero-light.php
@@ -6,7 +6,7 @@
  * Categories: icecubo-header
  */
 ?>
-<!-- wp:cover {"dimRatio":60,"gradient":"primary-to-transparent-rushly-radial","contentPosition":"center center","align":"full","style":{"spacing":{"padding":{"bottom":"5rem"}},"elements":{"link":{"color":{"text":"var:preset|color|darko-down"}},"heading":{"color":{"text":"var:preset|color|matching-6"}}}},"textColor":"darko-down"} -->
+<!-- wp:cover {"dimRatio":60,"gradient":"primary-to-transparent-rushly-radial","contentPosition":"center center","align":"full","style":{"spacing":{"padding":{"bottom":"5rem"}},"elements":{"link":{"color":{"text":"var:preset|color|darko-down"}}}},"textColor":"darko-down"} -->
 <div class="wp-block-cover alignfull has-darko-down-color has-text-color has-link-color" style="padding-bottom:5rem"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-60 has-background-dim has-background-gradient has-primary-to-transparent-rushly-radial-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","bottom":"var:preset|spacing|x-small"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-small)"><!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
 <div class="wp-block-group alignwide"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"flex"}} -->
@@ -24,8 +24,8 @@
 <div style="height:1.25rem" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
-<!-- wp:heading {"textAlign":"center","level":1,"className":"is-style-default"} -->
-<h1 class="wp-block-heading has-text-align-center is-style-default">IceCubo WordPress Theme</h1>
+<!-- wp:heading {"textAlign":"center","level":1,"style":{"elements":{"link":{"color":{"text":"var:preset|color|matching-6"}}}},"textColor":"matching-6","className":"is-style-default"} -->
+<h1 class="wp-block-heading has-text-align-center is-style-default has-matching-6-color has-text-color has-link-color">IceCubo WordPress Theme</h1>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"align":"center","fontSize":"large"} -->
@@ -33,8 +33,8 @@
 <!-- /wp:paragraph -->
 
 <!-- wp:media-text {"align":"","mediaPosition":"right","mediaType":"image","mediaSizeSlug":"large"} -->
-<div class="wp-block-media-text has-media-on-the-right is-stacked-on-mobile"><div class="wp-block-media-text__content"><!-- wp:heading -->
-<h2 class="wp-block-heading">It's a Block Time!</h2>
+<div class="wp-block-media-text has-media-on-the-right is-stacked-on-mobile"><div class="wp-block-media-text__content"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|matching-6"}}}},"textColor":"matching-6"} -->
+<h2 class="wp-block-heading has-matching-6-color has-text-color has-link-color">It's a Block Time!</h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"className":"is-style-default"} -->
@@ -42,7 +42,7 @@
 <!-- /wp:paragraph -->
 
 <!-- wp:buttons {"layout":{"type":"flex"}} -->
-<div class="wp-block-buttons"><!-- wp:button /--></div>
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"matching-6","textColor":"white","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}}} /--></div>
 <!-- /wp:buttons --></div><figure class="wp-block-media-text__media"><img src="<?php echo esc_url(get_theme_file_uri('/assets/img/ice-cubes.png')); ?>" alt="IceCubo placeholder image"/></figure></div>
 <!-- /wp:media-text --></div>
 <!-- /wp:group --></div></div>
--- patterns/hero-media-text-cta-light.php
+++ patterns/hero-media-text-cta-light.php
@@ -1,15 +1,15 @@
 <?php
 // phpcs:ignore
 /**
- * Title: Media and text as a call to action placeholder (Light background).
+ * Title: Media and text as a call to action placeholder. Focused background (Light).
  * Slug: icecubo/hero-media-text-cta-light
  * Categories: icecubo-hero
  */
 ?>
-<!-- wp:cover {"dimRatio":60,"gradient":"white-to-primary-radial-gradual","align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|large"}},"elements":{"link":{"color":{"text":"var:preset|color|darko-down"}},"heading":{"color":{"text":"var:preset|color|highlight-1"}}}},"textColor":"darko-down"} -->
+<!-- wp:cover {"dimRatio":60,"gradient":"white-to-primary-radial-gradual","align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|large"}},"elements":{"link":{"color":{"text":"var:preset|color|darko-down"}}}},"textColor":"darko-down"} -->
 <div class="wp-block-cover alignfull has-darko-down-color has-text-color has-link-color" style="padding-top:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--large)"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-60 has-background-dim has-background-gradient has-white-to-primary-radial-gradual-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"full","className":"is-light","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull is-light"><!-- wp:heading {"textAlign":"center"} -->
-<h2 class="wp-block-heading has-text-align-center"></h2>
+<div class="wp-block-group alignfull is-light"><!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|highlight-1"}}}},"textColor":"highlight-1"} -->
+<h2 class="wp-block-heading has-text-align-center has-highlight-1-color has-text-color has-link-color"></h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"align":"center","fontSize":"large"} -->
@@ -26,7 +26,7 @@
 <!-- /wp:paragraph -->
 
 <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
-<div class="wp-block-buttons"><!-- wp:button /--></div>
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"highlight-1"} /--></div>
 <!-- /wp:buttons --></div><figure class="wp-block-media-text__media"><img src="" alt=""/></figure></div>
 <!-- /wp:media-text --></div>
 <!-- /wp:group --></div></div>
