ddb63ad55dd7c36c627cd44f647e7f03f1c50c2d
Slobodan <sloooba@yahoo.com>
2023-10-22 20:54:35 +0200

    Change the design to 'img aside'

--- patterns/query-section-basic.php
+++ patterns/query-section-basic.php
@@ -1,38 +1,38 @@
 <?php
 // phpcs:ignore
 /**
- * Title: Posts wrapped in a group.
+ * Title: Posts wrapped in a group - Featured image aside.
  * Slug: icecubo/query-section-basic
  * Categories: icecubo-query-section
  * Inserter: true
  */
 ?>
 <!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|xxx-large"}}},"layout":{"type":"default"}} -->
-<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--xxx-large)"><!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
-<div class="wp-block-group alignwide"><!-- wp:paragraph {"className":"is-style-icecubo-mix-colors","fontSize":"xl"} -->
-<p class="is-style-icecubo-mix-colors has-xl-font-size">TECHNOLOGY</p>
-<!-- /wp:paragraph --></div>
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--xxx-large)"><!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|large"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide" style="margin-top:var(--wp--preset--spacing--large);margin-bottom:var(--wp--preset--spacing--large)"><!-- wp:heading {"className":"is-style-default","fontSize":"large"} -->
+<h2 class="wp-block-heading is-style-default has-large-font-size">TECHNOLOGY</h2>
+<!-- /wp:heading --></div>
 <!-- /wp:group -->
 
-<!-- wp:query {"queryId":0,"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"exclude","type":"constrained","inherit":false,"perPage":"3"},"align":"wide","layout":{"type":"default"}} -->
-<div class="wp-block-query alignwide"><!-- wp:post-template {"layout":{"type":"grid","columnCount":3}} -->
-<!-- wp:post-featured-image {"isLink":true,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}},"border":{"width":"0px","style":"none","radius":{"bottomLeft":"0px","bottomRight":"0px","topLeft":"3px","topRight":"3px"}}}} /-->
+<!-- wp:query {"query":{"perPage":"3","pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"exclude","inherit":false}} -->
+<div class="wp-block-query"><!-- wp:post-template {"layout":{"type":"grid","columnCount":3}} -->
+<!-- wp:columns {"verticalAlignment":"top"} -->
+<div class="wp-block-columns are-vertically-aligned-top"><!-- wp:column {"verticalAlignment":"top","width":"25%"} -->
+<div class="wp-block-column is-vertically-aligned-top" style="flex-basis:25%"><!-- wp:post-featured-image {"isLink":true,"aspectRatio":"auto","style":{"border":{"width":"4px"}}} /--></div>
+<!-- /wp:column -->
 
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","right":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|medium"}},"border":{"radius":{"bottomLeft":"3px","bottomRight":"3px","topLeft":"0px","topRight":"0px"}}},"backgroundColor":"white-ice"} -->
-<div class="wp-block-group has-white-ice-background-color has-background" style="border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:3px;border-bottom-right-radius:3px;padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:post-title {"isLink":true,"style":{"spacing":{"margin":{"top":"0"}}}} /-->
+<!-- wp:column {"verticalAlignment":"top","width":"75%"} -->
+<div class="wp-block-column is-vertically-aligned-top" style="flex-basis:75%"><!-- wp:post-title {"isLink":true,"style":{"spacing":{"margin":{"top":"0"}}},"fontSize":"medium"} /-->
 
-<!-- wp:group {"style":{"spacing":{"blockGap":".25rem"}},"layout":{"type":"flex"},"fontSize":"extra-small"} -->
-<div class="wp-block-group has-extra-small-font-size"><!-- wp:post-date /-->
+<!-- wp:post-date {"style":{"spacing":{"margin":{"top":"var:preset|spacing|xx-small"}}},"fontSize":"small"} /-->
 
-<!-- wp:paragraph -->
-<p>·</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:post-author {"showAvatar":false} /--></div>
-<!-- /wp:group -->
-
-<!-- wp:post-excerpt {"showMoreOnNewLine":false} /--></div>
-<!-- /wp:group -->
+<!-- wp:post-excerpt {"excerptLength":10,"style":{"spacing":{"margin":{"top":"var:preset|spacing|xx-small"}}}} /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
 <!-- /wp:post-template --></div>
-<!-- /wp:query --></div>
+<!-- /wp:query -->
+
+<!-- wp:paragraph {"align":"right"} -->
+<p class="has-text-align-right">→ Follow more from <a href="#">TECHNOLOGY</a></p>
+<!-- /wp:paragraph --></div>
 <!-- /wp:group -->
