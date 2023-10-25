734cdcc8fb382061a21213c8336c6490264a4d82
Slobodan <sloooba@yahoo.com>
2023-10-25 19:05:06 +0200

    Add the file

---
+++ patterns/query-section-columns-img-top.php
@@ -0,0 +1,56 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Two queries in columns - Featured image at top.
+ * Slug: icecubo/query-section-columns-img-top
+ * Categories: icecubo-query-section-columns
+ * Inserter: true
+ */
+?>
+<!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"className":"is-style-icecubo-aside-lines","fontSize":"large"} -->
+<h2 class="wp-block-heading is-style-icecubo-aside-lines has-large-font-size">BUSINESS</h2>
+<!-- /wp:heading -->
+
+<!-- wp:query {"query":{"perPage":4,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"exclude","inherit":false}} -->
+<div class="wp-block-query"><!-- wp:post-template -->
+<!-- wp:post-featured-image {"isLink":true,"height":"200px","style":{"border":{"width":"4px"}}} /-->
+
+<!-- wp:post-date {"fontSize":"small"} /-->
+
+<!-- wp:post-title {"isLink":true,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}}},"fontSize":"medium"} /-->
+
+<!-- wp:post-excerpt {"excerptLength":10,"style":{"spacing":{"margin":{"top":"var:preset|spacing|xxx-small"}}}} /-->
+<!-- /wp:post-template --></div>
+<!-- /wp:query -->
+
+<!-- wp:paragraph {"align":"right"} -->
+<p class="has-text-align-right">→More on <a href="#">BUSINESS</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"className":"is-style-icecubo-aside-lines","fontSize":"large"} -->
+<h2 class="wp-block-heading is-style-icecubo-aside-lines has-large-font-size">CULTURE</h2>
+<!-- /wp:heading -->
+
+<!-- wp:query {"query":{"perPage":4,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"exclude","inherit":false}} -->
+<div class="wp-block-query"><!-- wp:post-template -->
+<!-- wp:post-featured-image {"height":"200px","style":{"border":{"width":"4px"}}} /-->
+
+<!-- wp:post-date {"fontSize":"small"} /-->
+
+<!-- wp:post-title {"isLink":true,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}}},"fontSize":"medium"} /-->
+
+<!-- wp:post-excerpt {"excerptLength":10,"style":{"spacing":{"margin":{"top":"var:preset|spacing|xxx-small"}}}} /-->
+<!-- /wp:post-template --></div>
+<!-- /wp:query -->
+
+<!-- wp:paragraph {"align":"right"} -->
+<p class="has-text-align-right">→More on <a href="#">CULTURE</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
