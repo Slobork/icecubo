276c06d0924275ed138a6b51ca334e5489ada7b9
Slobodan <sloooba@yahoo.com>
2023-10-27 19:04:07 +0200

    Add the file

---
+++ patterns/page-template-home-2.php
@@ -0,0 +1,16 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Home page 2.
+ * Slug: icecubo/page-editor-home-2
+ * Categories: hidden
+ * Inserter: false
+ * Template Types: front-page, page
+ */
+?>
+<!-- wp:group {"tagName":"main","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|x-large"}}},"layout":{"type":"constrained"}} -->
+<main class="wp-block-group" style="padding-bottom:var(--wp--preset--spacing--x-large)">
+    <!-- wp:pattern {"slug":"icecubo/page-content-home-2"} /-->
+</main>
+<!-- /wp:group -->
+<!-- wp:template-part {"slug":"footer","theme":"icecubo","tagName":"footer"} /-->
