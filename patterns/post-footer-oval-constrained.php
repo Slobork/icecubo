91b39ffc02e3a0b89cba3243bcc41b8ed4f6bfd3
Slobodan <sloooba@yahoo.com>
2023-10-28 17:14:24 +0200

    Add the file

---
+++ patterns/post-footer-oval-constrained.php
@@ -0,0 +1,14 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Footer basic.
+ * Slug: icecubo/post-footer-oval-constrained
+ * Categories: hidden
+ * Inserter: false
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"},"fontSize":"extra-small","style":{"spacing":{"blockGap":"0px"}}} -->
+<div class="wp-block-group has-extra-small-font-size post-footer">
+    <!-- wp:pattern {"slug":"icecubo/post-footer-inner-content"} /-->
+</div>
+<!-- /wp:group -->
