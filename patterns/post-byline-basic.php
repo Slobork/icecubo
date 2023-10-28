9705ad9b2d65139be89967929468401f613128f1
Slobodan <sloooba@yahoo.com>
2023-10-28 17:15:38 +0200

    Update the content

--- patterns/post-byline-basic.php
+++ patterns/post-byline-basic.php
@@ -8,11 +8,11 @@
  */
 ?>
 <!-- wp:group {"style":{"spacing":{"blockGap":".25rem"}},"layout":{"type":"flex"},"fontSize":"extra-small"} -->
-<div class="wp-block-group has-extra-small-font-size">
-    <!-- wp:post-date {"backgroundColor":"white-ice","className":"is-style-icecubo-oval-post-byline"} /-->
-    <!-- wp:paragraph -->
-    <p>·</p>
-    <!-- /wp:paragraph -->
-    <!-- wp:post-author {"showAvatar":false,"backgroundColor":"white-ice","className":"is-style-icecubo-oval-post-byline"} /-->
-</div>
+<div class="wp-block-group has-extra-small-font-size"><!-- wp:post-date /-->
+
+<!-- wp:paragraph -->
+<p>·</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:post-author {"showAvatar":false} /--></div>
 <!-- /wp:group -->
