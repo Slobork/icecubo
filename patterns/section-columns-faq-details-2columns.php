1941a838d4c7491c564edab9eeaf71c9ee2e3308
Slobodan <sloooba@yahoo.com>
2023-11-04 14:31:39 +0100

    Update Title & text in the left column

--- patterns/section-columns-faq-details-2columns.php
+++ patterns/section-columns-faq-details-2columns.php
@@ -1,38 +1,38 @@
 <?php
 // phpcs:ignore
 /**
- * Title: Columns - FAQ with details block - Tip: Duplicate the column with preferred style and delete other columns
+ * Title: Columns - FAQ with details block - Large vertical title on the left
  * Slug: icecubo/section-columns-faq-details-2columns
  * Categories: icecubo-section
  */
 ?>
 <!-- wp:columns {"verticalAlignment":null} -->
-<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"top"} -->
-<div class="wp-block-column is-vertically-aligned-top"><!-- wp:heading {"textAlign":"center"} -->
-<h2 class="wp-block-heading has-text-align-center">Frequently Asked Questions</h2>
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"style":{"typography":{"writingMode":"vertical-rl","fontSize":"9rem"}}} -->
+<h2 class="wp-block-heading" style="font-size:9rem;writing-mode:vertical-rl">FAQ</h2>
 <!-- /wp:heading --></div>
 <!-- /wp:column -->
 
 <!-- wp:column -->
-<div class="wp-block-column"><!-- wp:details {"showContent":true,"summary":"Lorem ipsum dolor sit amet?","className":"is-style-icecubo-details-square"} -->
+<div class="wp-block-column"><!-- wp:details {"showContent":true,"className":"is-style-icecubo-details-square"} -->
 <details class="wp-block-details is-style-icecubo-details-square" open><summary>Lorem ipsum dolor sit amet?</summary><!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0.5rem"}}}} -->
 <p style="margin-top:0.5rem">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 <!-- /wp:paragraph --></details>
 <!-- /wp:details -->
 
-<!-- wp:details {"summary":"Lorem ipsum dolor sit amet?","className":"is-style-icecubo-details-square"} -->
+<!-- wp:details {"className":"is-style-icecubo-details-square"} -->
 <details class="wp-block-details is-style-icecubo-details-square"><summary>Lorem ipsum dolor sit amet?</summary><!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0.5rem"}}}} -->
 <p style="margin-top:0.5rem">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 <!-- /wp:paragraph --></details>
 <!-- /wp:details -->
 
-<!-- wp:details {"summary":"Lorem ipsum dolor sit amet?","className":"is-style-icecubo-details-square"} -->
+<!-- wp:details {"className":"is-style-icecubo-details-square"} -->
 <details class="wp-block-details is-style-icecubo-details-square"><summary>Lorem ipsum dolor sit amet?</summary><!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0.5rem"}}}} -->
 <p style="margin-top:0.5rem">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 <!-- /wp:paragraph --></details>
 <!-- /wp:details -->
 
-<!-- wp:details {"summary":"Lorem ipsum dolor sit amet?","className":"is-style-icecubo-details-square"} -->
+<!-- wp:details {"className":"is-style-icecubo-details-square"} -->
 <details class="wp-block-details is-style-icecubo-details-square"><summary>Lorem ipsum dolor sit amet?</summary><!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0.5rem"}}}} -->
 <p style="margin-top:0.5rem">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 <!-- /wp:paragraph --></details>
