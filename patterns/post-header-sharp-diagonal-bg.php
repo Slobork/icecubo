c1617d47138b6d094a48b901d3c6dcad931373ee
Slobodan <sloooba@yahoo.com>
2023-11-09 11:44:00 +0100

    Add min-height to wrapper group

--- patterns/post-header-sharp-diagonal-bg.php
+++ patterns/post-header-sharp-diagonal-bg.php
@@ -6,8 +6,8 @@
  * Categories: icecubo-post-header
  */
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|xxx-large"},"blockGap":"var:preset|spacing|small"}},"gradient":"primary-to-transparent-linear-sharp-corner","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull has-primary-to-transparent-linear-sharp-corner-gradient-background has-background" style="padding-top:var(--wp--preset--spacing--xxx-large)"><!-- wp:post-title {"level":1,"style":{"spacing":{"margin":{"top":"var:preset|spacing|small"}}}} /-->
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|xxx-large"},"blockGap":"var:preset|spacing|small"},"dimensions":{"minHeight":"900px"}},"gradient":"primary-to-transparent-linear-sharp-corner","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-primary-to-transparent-linear-sharp-corner-gradient-background has-background" style="min-height:900px;padding-top:var(--wp--preset--spacing--xxx-large)"><!-- wp:post-title {"level":1,"style":{"spacing":{"margin":{"top":"var:preset|spacing|small"}}}} /-->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"0.75rem"}},"layout":{"type":"flex"},"fontSize":"extra-small"} -->
 <div class="wp-block-group has-extra-small-font-size"><!-- wp:post-date {"backgroundColor":"white-ice","className":"is-style-icecubo-oval-post-byline"} /-->
