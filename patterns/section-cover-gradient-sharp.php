a207e329bf73b8be2d3ab32749df116d9fafca20
Slobodan <sloooba@yahoo.com>
2024-01-03 19:14:21 +0100

    Change gradient '-to-transparent'

--- patterns/section-cover-gradient-sharp.php
+++ patterns/section-cover-gradient-sharp.php
@@ -6,8 +6,8 @@
  * Categories: icecubo-section
  */
 ?>
-<!-- wp:cover {"gradient":"primary-to-white-linear-sharp-corner","isDark":false} -->
-<div class="wp-block-cover is-light"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim has-background-gradient has-primary-to-white-linear-sharp-corner-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small"}}},"layout":{"type":"constrained"}} -->
+<!-- wp:cover {"gradient":"primary-to-transparent-linear-sharp-corner","isDark":false} -->
+<div class="wp-block-cover is-light"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim has-background-gradient has-primary-to-transparent-linear-sharp-corner-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small)"><!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column -->
 <div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}}},"backgroundColor":"white"} -->
