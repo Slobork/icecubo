d7942bb625ad388f54925b60e92da9d90d875337
Slobodan <sloooba@yahoo.com>
2024-01-05 20:16:33 +0100

    Change the headings color

--- patterns/hero-media-text-cta-left-aligned-light.php
+++ patterns/hero-media-text-cta-left-aligned-light.php
@@ -8,8 +8,8 @@
 ?>
 <!-- wp:cover {"dimRatio":60,"gradient":"primary-to-transparent-rushly-radial","align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|large"}},"elements":{"link":{"color":{"text":"var:preset|color|darko-down"}}}},"textColor":"darko-down"} -->
 <div class="wp-block-cover alignfull has-darko-down-color has-text-color has-link-color" style="padding-top:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--large)"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-60 has-background-dim has-background-gradient has-primary-to-transparent-rushly-radial-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"full","className":"is-light","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull is-light"><!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|highlight-1"}}}},"textColor":"highlight-1"} -->
-<h2 class="wp-block-heading has-text-align-center has-highlight-1-color has-text-color has-link-color"></h2>
+<div class="wp-block-group alignfull is-light"><!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|classy-3"}}}},"textColor":"classy-3"} -->
+<h2 class="wp-block-heading has-text-align-center has-classy-3-color has-text-color has-link-color"></h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"align":"center","fontSize":"large"} -->
@@ -21,8 +21,8 @@
 <!-- /wp:spacer -->
 
 <!-- wp:media-text {"align":"","mediaPosition":"right","mediaType":"image","mediaSizeSlug":"large"} -->
-<div class="wp-block-media-text has-media-on-the-right is-stacked-on-mobile"><div class="wp-block-media-text__content"><!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|highlight-1"}}}},"textColor":"highlight-1"} -->
-<h3 class="wp-block-heading has-highlight-1-color has-text-color has-link-color"></h3>
+<div class="wp-block-media-text has-media-on-the-right is-stacked-on-mobile"><div class="wp-block-media-text__content"><!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|classy-3"}}}},"textColor":"classy-3"} -->
+<h3 class="wp-block-heading has-classy-3-color has-text-color has-link-color"></h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"className":"is-style-default"} -->
@@ -30,7 +30,7 @@
 <!-- /wp:paragraph -->
 
 <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
-<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"highlight-1","textColor":"white","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}}} /--></div>
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"classy-3","textColor":"white","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}}} /--></div>
 <!-- /wp:buttons --></div><figure class="wp-block-media-text__media"><img src="" alt=""/></figure></div>
 <!-- /wp:media-text --></div>
 <!-- /wp:group --></div></div>
