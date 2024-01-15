485d735536f0e6de3398f11ee093907eeca27402
Slobodan <sloooba@yahoo.com>
2024-01-15 21:15:46 +0100

    Add images placeholder

--- patterns/boxes-with-cover-img.php
+++ patterns/boxes-with-cover-img.php
@@ -1,17 +1,21 @@
 <?php
 // phpcs:ignore
 /**
- * Title: Basic group boxes as columns with cover background (Optionally add a cover image).
+ * Title: Boxes with images set as columns with cover background (Optionally add a cover image).
  * Slug: icecubo/collection-basic
  * Categories: icecubo-box
  */
 ?>
 <!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column -->
-<div class="wp-block-column"><!-- wp:cover {"dimRatio":50,"overlayColor":"white-ice","minHeight":500,"isDark":false,"align":"center","textColor":"darko-down-bottom"} -->
-<div class="wp-block-cover aligncenter is-light has-darko-down-bottom-color has-text-color" style="min-height:500px"><span aria-hidden="true" class="wp-block-cover__background has-white-ice-background-color has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":3} -->
+<div class="wp-block-column"><!-- wp:cover {"dimRatio":50,"overlayColor":"white-ice","minHeight":430,"isDark":false,"align":"center","textColor":"darko-down-bottom"} -->
+<div class="wp-block-cover aligncenter is-light has-darko-down-bottom-color has-text-color" style="min-height:430px"><span aria-hidden="true" class="wp-block-cover__background has-white-ice-background-color has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":3} -->
 <h3 class="wp-block-heading has-text-align-center"></h3>
 <!-- /wp:heading -->
+
+<!-- wp:image {"align":"center","aspectRatio":"3/2","scale":"cover","className":"is-style-rounded"} -->
+<figure class="wp-block-image aligncenter is-style-rounded"><img alt="" style="aspect-ratio:3/2;object-fit:cover"/></figure>
+<!-- /wp:image -->
 
 <!-- wp:paragraph {"align":"center"} -->
 <p class="has-text-align-center">Tip: You can add an image to the parent cover block or remove its opacity.</p>
@@ -26,10 +30,14 @@
 <!-- /wp:column -->
 
 <!-- wp:column -->
-<div class="wp-block-column"><!-- wp:cover {"dimRatio":50,"overlayColor":"white-ice","minHeight":500,"isDark":false,"align":"center","textColor":"darko-down-bottom"} -->
-<div class="wp-block-cover aligncenter is-light has-darko-down-bottom-color has-text-color" style="min-height:500px"><span aria-hidden="true" class="wp-block-cover__background has-white-ice-background-color has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":3} -->
+<div class="wp-block-column"><!-- wp:cover {"dimRatio":50,"overlayColor":"white-ice","minHeight":430,"isDark":false,"align":"center","textColor":"darko-down-bottom"} -->
+<div class="wp-block-cover aligncenter is-light has-darko-down-bottom-color has-text-color" style="min-height:430px"><span aria-hidden="true" class="wp-block-cover__background has-white-ice-background-color has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":3} -->
 <h3 class="wp-block-heading has-text-align-center"></h3>
 <!-- /wp:heading -->
+
+<!-- wp:image {"align":"center","aspectRatio":"3/2","scale":"cover","className":"is-style-rounded"} -->
+<figure class="wp-block-image aligncenter is-style-rounded"><img alt="" style="aspect-ratio:3/2;object-fit:cover"/></figure>
+<!-- /wp:image -->
 
 <!-- wp:paragraph {"align":"center"} -->
 <p class="has-text-align-center">Tip: You can add an image to the parent cover block or remove its opacity.</p>
@@ -44,10 +52,14 @@
 <!-- /wp:column -->
 
 <!-- wp:column -->
-<div class="wp-block-column"><!-- wp:cover {"dimRatio":50,"overlayColor":"white-ice","minHeight":500,"isDark":false,"align":"center","textColor":"darko-down-bottom"} -->
-<div class="wp-block-cover aligncenter is-light has-darko-down-bottom-color has-text-color" style="min-height:500px"><span aria-hidden="true" class="wp-block-cover__background has-white-ice-background-color has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":3} -->
+<div class="wp-block-column"><!-- wp:cover {"dimRatio":50,"overlayColor":"white-ice","minHeight":430,"isDark":false,"align":"center","textColor":"darko-down-bottom"} -->
+<div class="wp-block-cover aligncenter is-light has-darko-down-bottom-color has-text-color" style="min-height:430px"><span aria-hidden="true" class="wp-block-cover__background has-white-ice-background-color has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":3} -->
 <h3 class="wp-block-heading has-text-align-center"></h3>
 <!-- /wp:heading -->
+
+<!-- wp:image {"align":"center","aspectRatio":"3/2","scale":"cover","className":"is-style-rounded"} -->
+<figure class="wp-block-image aligncenter is-style-rounded"><img alt="" style="aspect-ratio:3/2;object-fit:cover"/></figure>
+<!-- /wp:image -->
 
 <!-- wp:paragraph {"align":"center"} -->
 <p class="has-text-align-center">Tip: You can add an image to the parent cover block or remove its opacity.</p>
