c14ddd5b6315405ba93a1fbd40a2f972654795e3
Slobodan <sloooba@yahoo.com>
2024-01-23 14:11:52 +0100

    Add the file

---
+++ patterns/box-cta-stack-bordered.php
@@ -0,0 +1,23 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Call to action box - Stack (minimal border)
+ * Slug: icecubo/box-cta-stack-bordered
+ * Categories: icecubo-box
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|large","right":"var:preset|spacing|large","left":"var:preset|spacing|large"}},"border":{"radius":"20px","width":"2px"}},"borderColor":"primary","className":"with-text-alignment","layout":{"type":"constrained"}} -->
+<div class="wp-block-group with-text-alignment has-border-color has-primary-border-color" style="border-width:2px;border-radius:20px;padding-top:var(--wp--preset--spacing--large);padding-right:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--large);padding-left:var(--wp--preset--spacing--large)"><!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+<p class="has-text-align-center has-large-font-size">Call-to-Action</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">Get it now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
---
+++ patterns/box-group-bordered.php
@@ -0,0 +1,29 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Basic group box with border minimal
+ * Slug: icecubo/box-in-group-bordered
+ * Categories: icecubo-box
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|x-large","top":"var:preset|spacing|x-large","right":"var:preset|spacing|medium","left":"var:preset|spacing|medium"},"blockGap":"var:preset|spacing|large"},"border":{"radius":"20px","width":"2px"}},"borderColor":"primary","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-border-color has-primary-border-color" style="border-width:2px;border-radius:20px;padding-top:var(--wp--preset--spacing--x-large);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-large);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|xxx-small"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>SUBTITLE</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading">Lorem ipsum dolor</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra justo nec ultrices dui sapien. Bibendum ut tristique et egestas quis. Nulla posuere sollicitudin aliquam ultrices. Consequat id porta nibh venenatis. Vitae purus faucibus ornare suspendisse sed nisi. Pharetra pharetra massa massa ultricies mi quis hendrerit. Aliquam id diam maecenas ultricies mi eget. Tellus in hac habitasse platea dictumst vestibulum. Lorem mollis aliquam ut porttitor leo a diam. Duis at tellus at urna condimentum mattis pellentesque. Sit amet nisl suscipit adipiscing bibendum est ultricies integer. Ut tristique et egestas quis. Diam in arcu cursus euismod quis. Amet cursus sit amet dictum. Eu lobortis elementum nibh tellus molestie nunc non blandit massa. Eget mauris pharetra et ultrices neque ornare aenean euismod elementum. Cursus in hac habitasse platea dictumst quisque. Senectus et netus et malesuada fames ac turpis. Non consectetur a erat nam.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"gradient":"darko-to-primary-linear-gradual-aside"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-darko-to-primary-linear-gradual-aside-gradient-background has-background wp-element-button">Learn more →</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
