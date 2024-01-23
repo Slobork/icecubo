9045a86ba43b4e0961eedc390795594e230d2775
Slobodan <sloooba@yahoo.com>
2024-01-23 12:55:38 +0100

    Change the pdding on the wrapper group

--- patterns/post-header-fading-bg.php
+++ patterns/post-header-fading-bg.php
@@ -6,8 +6,8 @@
  * Categories: icecubo-post-header
  */
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|xxx-large"},"blockGap":"var:preset|spacing|small"}},"gradient":"primary-to-transparent-rushly-radial","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull has-primary-to-transparent-rushly-radial-gradient-background has-background" style="padding-top:var(--wp--preset--spacing--xxx-large)"><!-- wp:post-title {"level":1,"style":{"spacing":{"margin":{"top":"var:preset|spacing|small"}}}} /-->
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|medium"},"blockGap":"var:preset|spacing|small"}},"gradient":"primary-to-transparent-rushly-radial","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-primary-to-transparent-rushly-radial-gradient-background has-background" style="padding-top:var(--wp--preset--spacing--medium)"><!-- wp:post-title {"level":1,"style":{"spacing":{"margin":{"top":"var:preset|spacing|small"}}}} /-->
 
     <!-- wp:pattern {"slug":"icecubo/post-byline-oval"} /-->
 
