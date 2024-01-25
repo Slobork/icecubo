dff503f456231bbcc6dee1fbb203f44b7ccac88d
Slobodan <sloooba@yahoo.com>
2024-01-25 15:03:01 +0100

    Add the file

---
+++ patterns/basic-testimonials-aside-img.php
@@ -0,0 +1,45 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Testimonials in columns with image aside.
+ * Slug: icecubo/basic-testimonials-aside-image
+ * Categories: icecubo-basic-testimonials
+ */
+?>
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+<div class="wp-block-group"><!-- wp:image {"aspectRatio":"1","scale":"cover","className":"is-style-rounded"} -->
+<figure class="wp-block-image is-style-rounded"><img alt="" style="aspect-ratio:1;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"5px"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>Cursus in hac habitasse platea dictumst quisque. Senectus et netus et malesuada fames ac turpis.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">— John Doe, JDinc</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+<div class="wp-block-group"><!-- wp:image {"aspectRatio":"1","scale":"cover","className":"is-style-rounded"} -->
+<figure class="wp-block-image is-style-rounded"><img alt="" style="aspect-ratio:1;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"5px"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>Cursus in hac habitasse platea dictumst quisque. Senectus et netus et malesuada fames ac turpis.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">— John Doe, JDinc</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
