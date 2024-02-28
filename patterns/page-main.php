efa6f124b6e25c7378ba5147c7060104b3d35ab3
Slobodan <sloooba@yahoo.com>
2024-02-28 17:19:09 +0100

    Update padding-bottom on the #main

--- patterns/page-main.php
+++ patterns/page-main.php
@@ -7,8 +7,8 @@
  * Inserter: false
  */
 ?>
-<!-- wp:group {"tagName":"main","style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","bottom":"var:preset|spacing|x-large"},"margin":{"top":"0"}}}} -->
-<main class="wp-block-group" style="margin-top:0;padding-top:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-large)">
+<!-- wp:group {"tagName":"main","style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","bottom":"3.5rem"},"margin":{"top":"0"}}}} -->
+<main class="wp-block-group" style="margin-top:0;padding-top:var(--wp--preset--spacing--medium);padding-bottom:3.5rem">
     <!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|small"},"blockGap":"var:preset|spacing|xxx-small"}},"layout":{"type":"constrained"}} -->
     <div class="wp-block-group" style="padding-bottom:var(--wp--preset--spacing--small)">
         <!-- wp:post-title {"level":1} /-->
