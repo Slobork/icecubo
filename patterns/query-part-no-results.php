f82e055c03e08a3aa2deb1d362ee18e38871bea0
Slobodan <sloooba@yahoo.com>
2024-04-23 13:08:49 +0200

    Rename the file & add the text 'No results.'

--- patterns/query-part-no-results.php
+++ patterns/query-part-no-results.php
@@ -9,6 +9,6 @@
 ?>
 <!-- wp:query-no-results -->
 <!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results."} -->
-<p></p>
+<p>No results.</p>
 <!-- /wp:paragraph -->
 <!-- /wp:query-no-results -->
