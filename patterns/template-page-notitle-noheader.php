28918bdb602706fa99ec029c0c85098f24b017d5
Slobodan <sloooba@yahoo.com>
2024-04-25 17:06:50 +0200

    Add the file

---
+++ patterns/template-page-notitle-noheader.php
@@ -0,0 +1,16 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Content for the page template without a title and site header.
+ * Slug: icecubo/template-page-notitle-noheader
+ * Categories: hidden
+ * Inserter:false
+ */
+?>
+<!-- wp:group {"tagName":"main","style":{"spacing":{"padding":{"bottom":"3.5rem"}}}} -->
+<main class="wp-block-group" style="padding-bottom:3.5rem">
+    <!-- wp:post-content {"layout":{"type":"constrained"}} /-->
+    <!-- wp:pattern {"slug":"icecubo/comments"} /-->
+</main>
+<!-- /wp:group -->
+<!-- wp:template-part {"slug":"footer","tagName":"footer","className":"site-footer"} /-->
