6255ea9bfaaeebf946da1beeaf66fe1c577595be
Slobodan <sloooba@yahoo.com>
2024-04-29 19:49:06 +0200

    Add the file

---
+++ patterns/box-cta-cover-focus-2.php
@@ -0,0 +1,35 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Call to action box - In a focused cover (2)
+ * Slug: icecubo/box-cta-cover-focus-2
+ * Categories: icecubo-box
+ */
+?>
+<!-- wp:cover {"dimRatio":50,"isUserOverlayColor":true,"gradient":"white-to-primary-radial-gradual","isDark":false,"metadata":{"categories":["icecubo-section"],"patternName":"icecubo/section-cover-numbers","name":"Cover - accent numbers."},"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|large","right":"var:preset|spacing|large","left":"var:preset|spacing|large"}},"border":{"radius":"20px"}},"className":"is-light"} -->
+<div class="wp-block-cover alignwide is-light" style="border-radius:20px;padding-top:var(--wp--preset--spacing--large);padding-right:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--large);padding-left:var(--wp--preset--spacing--large)"><span aria-hidden="true" class="wp-block-cover__background has-background-dim has-background-gradient has-white-to-primary-radial-gradual-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center","width":"55%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:55%"><!-- wp:heading {"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}}}} -->
+<h2 class="wp-block-heading" style="margin-top:var(--wp--preset--spacing--x-small)">Lorem ipsum dolor sit amet consectetur!</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|xxx-small"}}}} -->
+<p style="margin-top:var(--wp--preset--spacing--xxx-small)">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:cover {"isUserOverlayColor":true,"minHeight":430,"minHeightUnit":"px","gradient":"transparent-to-primary-radial-sharp","metadata":{"categories":["icecubo-box"],"patternName":"icecubo/box-cta-circled-cover","name":"Call to action box - In a circled cover"},"style":{"color":{"duotone":"unset"},"elements":{"link":{"color":{"text":"var:preset|color|darko"}}},"spacing":{"margin":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|large"}}},"textColor":"darko","className":"is-style-default"} -->
+<div class="wp-block-cover is-style-default has-darko-color has-text-color has-link-color" style="margin-top:var(--wp--preset--spacing--large);margin-bottom:var(--wp--preset--spacing--large);min-height:430px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim has-background-gradient has-transparent-to-primary-radial-sharp-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"Write title…","backgroundColor":"highlight-2","className":"is-style-icecubo-tearbox-right-p","fontSize":"large","fontFamily":"lato"} -->
+<p class="has-text-align-center is-style-icecubo-tearbox-right-p has-highlight-2-background-color has-background has-lato-font-family has-large-font-size">Check availability for the next project.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"textColor":"white","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"className":"is-style-fill"} -->
+<div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-white-color has-text-color has-link-color wp-element-button">CALL US</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div></div>
+<!-- /wp:cover -->
