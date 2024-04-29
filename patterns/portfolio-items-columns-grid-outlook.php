d13a542b401d6c6a758dcf89288e8fc2d40c11cd
Slobodan <sloooba@yahoo.com>
2024-04-29 14:25:03 +0200

    Add the file

---
+++ patterns/portfolio-items-columns-grid-outlook.php
@@ -0,0 +1,101 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Portfolio items in columns composing a grid outlook.
+ * Slug: icecubo/portfolio-items-columns-grid-outlook
+ * Categories: icecubo-portfolio
+ */
+?>
+<!-- wp:columns {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|large","left":"var:preset|spacing|large"},"blockGap":{"left":"5px"},"margin":{"top":"5px","bottom":"5px"}}}} -->
+<div class="wp-block-columns alignfull" style="margin-top:5px;margin-bottom:5px;padding-right:var(--wp--preset--spacing--large);padding-left:var(--wp--preset--spacing--large)"><!-- wp:column {"width":"60%","style":{"spacing":{"blockGap":"5px"}}} -->
+<div class="wp-block-column" style="flex-basis:60%"><!-- wp:cover {"url":"<?php echo esc_url(get_theme_file_uri()); ?>/assets/img/ice-orange-straw.jpeg","dimRatio":90,"isUserOverlayColor":true,"gradient":"primary-to-transparent-linear-gradual","style":{"border":{"radius":"20px"},"spacing":{"padding":{"right":"var:preset|spacing|medium","left":"var:preset|spacing|medium"}}},"className":"is-light"} -->
+<div class="wp-block-cover is-light" style="border-radius:20px;padding-right:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-90 has-background-dim wp-block-cover__gradient-background has-background-gradient has-primary-to-transparent-linear-gradual-gradient-background"></span><img class="wp-block-cover__image-background" alt="Ice cubes and orange slices in a glass" src="<?php echo esc_url(get_theme_file_uri('/assets/img/ice-orange-straw.jpeg')); ?>" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center","width":"55%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:55%"><!-- wp:heading {"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}},"typography":{"textTransform":"uppercase"},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white","fontSize":"xxl"} -->
+<h2 class="wp-block-heading has-white-color has-text-color has-link-color has-xxl-font-size" style="margin-top:var(--wp--preset--spacing--x-small);text-transform:uppercase"></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|xxx-small"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<p class="has-white-color has-text-color has-link-color" style="margin-top:var(--wp--preset--spacing--xxx-small)">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"gradient":"primary-to-darko-gradual","width":75} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-75"><a class="wp-block-button__link has-primary-to-darko-gradual-gradient-background has-background wp-element-button">Learn more</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:cover {"url":"<?php echo esc_url(get_theme_file_uri()); ?>/assets/img/ice-orange-straw.jpeg","dimRatio":90,"isUserOverlayColor":true,"gradient":"primary-to-transparent-linear-gradual","style":{"border":{"radius":"20px"},"spacing":{"padding":{"right":"var:preset|spacing|medium","left":"var:preset|spacing|medium"}}},"className":"is-light"} -->
+<div class="wp-block-cover is-light" style="border-radius:20px;padding-right:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-90 has-background-dim wp-block-cover__gradient-background has-background-gradient has-primary-to-transparent-linear-gradual-gradient-background"></span><img class="wp-block-cover__image-background" alt="Ice cubes and orange slices in a glass" src="<?php echo esc_url(get_theme_file_uri('/assets/img/ice-orange-straw.jpeg')); ?>" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center","width":"55%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:55%"><!-- wp:heading {"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}},"typography":{"textTransform":"uppercase"},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white","fontSize":"xxl"} -->
+<h2 class="wp-block-heading has-white-color has-text-color has-link-color has-xxl-font-size" style="margin-top:var(--wp--preset--spacing--x-small);text-transform:uppercase"></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|xxx-small"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<p class="has-white-color has-text-color has-link-color" style="margin-top:var(--wp--preset--spacing--xxx-small)">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"gradient":"primary-to-darko-gradual","width":75} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-75"><a class="wp-block-button__link has-primary-to-darko-gradual-gradient-background has-background wp-element-button">Learn more</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"60%","style":{"spacing":{"blockGap":"5px"}}} -->
+<div class="wp-block-column" style="flex-basis:60%"><!-- wp:cover {"url":"<?php echo esc_url(get_theme_file_uri()); ?>/assets/img/ice-orange-straw.jpeg","dimRatio":90,"isUserOverlayColor":true,"gradient":"primary-to-transparent-linear-gradual","style":{"border":{"radius":"20px"},"spacing":{"padding":{"right":"var:preset|spacing|medium","left":"var:preset|spacing|medium"}}},"className":"is-light"} -->
+<div class="wp-block-cover is-light" style="border-radius:20px;padding-right:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-90 has-background-dim wp-block-cover__gradient-background has-background-gradient has-primary-to-transparent-linear-gradual-gradient-background"></span><img class="wp-block-cover__image-background" alt="Ice cubes and orange slices in a glass" src="<?php echo esc_url(get_theme_file_uri('/assets/img/ice-orange-straw.jpeg')); ?>" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center","width":"55%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:55%"><!-- wp:heading {"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}},"typography":{"textTransform":"uppercase"},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white","fontSize":"xxl"} -->
+<h2 class="wp-block-heading has-white-color has-text-color has-link-color has-xxl-font-size" style="margin-top:var(--wp--preset--spacing--x-small);text-transform:uppercase"></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|xxx-small"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<p class="has-white-color has-text-color has-link-color" style="margin-top:var(--wp--preset--spacing--xxx-small)">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"gradient":"primary-to-darko-gradual","width":75} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-75"><a class="wp-block-button__link has-primary-to-darko-gradual-gradient-background has-background wp-element-button">Learn more</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:cover {"url":"<?php echo esc_url(get_theme_file_uri()); ?>/assets/img/ice-orange-straw.jpeg","dimRatio":90,"isUserOverlayColor":true,"gradient":"primary-to-transparent-linear-gradual","style":{"border":{"radius":"20px"},"spacing":{"padding":{"right":"var:preset|spacing|medium","left":"var:preset|spacing|medium"}}},"className":"is-light"} -->
+<div class="wp-block-cover is-light" style="border-radius:20px;padding-right:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-90 has-background-dim wp-block-cover__gradient-background has-background-gradient has-primary-to-transparent-linear-gradual-gradient-background"></span><img class="wp-block-cover__image-background" alt="Ice cubes and orange slices in a glass" src="<?php echo esc_url(get_theme_file_uri('/assets/img/ice-orange-straw.jpeg')); ?>" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center","width":"55%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:55%"><!-- wp:heading {"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}},"typography":{"textTransform":"uppercase"},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white","fontSize":"xxl"} -->
+<h2 class="wp-block-heading has-white-color has-text-color has-link-color has-xxl-font-size" style="margin-top:var(--wp--preset--spacing--x-small);text-transform:uppercase"></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|xxx-small"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<p class="has-white-color has-text-color has-link-color" style="margin-top:var(--wp--preset--spacing--xxx-small)">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"gradient":"primary-to-darko-gradual","width":75} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-75"><a class="wp-block-button__link has-primary-to-darko-gradual-gradient-background has-background wp-element-button">Learn more</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
