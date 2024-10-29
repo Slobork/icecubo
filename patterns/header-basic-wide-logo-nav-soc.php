22cedcac2318213cf84fd8f372fa38626a2f9dba
Slobodan <sloooba@yahoo.com>
2024-10-29 11:58:29 +0100

    Rename and adjust the wrapper group padding

---
+++ patterns/header-basic-wide-logo-nav-soc.php
@@ -0,0 +1,26 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Spread elements: Logo - nav - social icons.
+ * Slug: icecubo/header-basic-wide-logo-nav-soc
+ * Categories: icecubo-header
+ * Block Types: core/template-part/header
+ */
+?>
+<!-- wp:group {"align":"full","className":"is-style-default","style":{"spacing":{"padding":{"top":"var:preset|spacing|xxx-small","bottom":"var:preset|spacing|xx-small"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-style-default" style="padding-top:var(--wp--preset--spacing--xxx-small);padding-bottom:var(--wp--preset--spacing--xx-small)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"flex","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:site-logo /--></div>
+<!-- /wp:group -->
+
+<!-- wp:navigation {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"flex","justifyContent":"center","orientation":"horizontal","flexWrap":"nowrap"}} /-->
+
+<!-- wp:social-links {"className":"is-style-default"} -->
+<ul class="wp-block-social-links is-style-default"><!-- wp:social-link {"url":"#","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"#","service":"x"} /-->
+
+<!-- wp:social-link {"url":"#","service":"linkedin"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
