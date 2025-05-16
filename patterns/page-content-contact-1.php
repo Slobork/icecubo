eeb6b378a2df835582931bbca07cc05069090f01
Slobodan <sloooba@yahoo.com>
2025-05-16 16:27:51 +0200

    Add the file

---
+++ patterns/page-content-contact-1.php
@@ -0,0 +1,61 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Contact page content (1).
+ * Slug: icecubo/page-content-contact-1
+ * Categories: icecubo-page-contact
+ */
+?>
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|xxx-small"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"fontSize":"large"} -->
+<h2 class="wp-block-heading has-large-font-size">Call us</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>(555) 555-555</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|xxx-small"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"fontSize":"large"} -->
+<h2 class="wp-block-heading has-large-font-size">Find us</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Dolor Sit, 567 89</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|xxx-small"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"fontSize":"large"} -->
+<h2 class="wp-block-heading has-large-font-size">Follow us</h2>
+<!-- /wp:heading -->
+
+<!-- wp:social-links {"iconColor":"darko-transit","iconColorValue":"#171f29","iconBackgroundColor":"transparent","iconBackgroundColorValue":"transparent","className":"is-style-icecubo-social-outline","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|xxx-small"}}},"layout":{"type":"flex","justifyContent":"left"}} -->
+<ul class="wp-block-social-links has-icon-color has-icon-background-color is-style-icecubo-social-outline"><!-- wp:social-link {"url":"#","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"#","service":"linkedin"} /-->
+
+<!-- wp:social-link {"url":"#","service":"instagram"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"66.66%"} -->
+<div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:heading -->
+<h2 class="wp-block-heading">Have a Question?</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>You should put a contact form beneath. First, you should install a plugin, for example "Contact form 7", then insert its shortcode. Also remove this paragraph.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:shortcode /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+<!-- wp:spacer {"height":"80px"} -->
+<div style="height:80px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+<!-- wp:pattern {"slug":"icecubo/box-summary"} /-->
---
+++ patterns/page-content-contact-2.php
@@ -0,0 +1,54 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Contact page content (2).
+ * Slug: icecubo/page-content-contact-2
+ * Categories: icecubo-page-contact
+ */
+?>
+<!-- wp:group {"layout":{"type":"grid","columnCount":3,"minimumColumnWidth":null}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|xxx-small","padding":{"right":"var:preset|spacing|xx-small","left":"var:preset|spacing|xx-small","top":"var:preset|spacing|xx-small","bottom":"var:preset|spacing|xx-small"}}},"backgroundColor":"primary-light","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-primary-light-background-color has-background" style="padding-top:var(--wp--preset--spacing--xx-small);padding-right:var(--wp--preset--spacing--xx-small);padding-bottom:var(--wp--preset--spacing--xx-small);padding-left:var(--wp--preset--spacing--xx-small)"><!-- wp:heading {"fontSize":"large"} -->
+<h2 class="wp-block-heading has-large-font-size">Call us</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>(555) 555-555</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|xxx-small","padding":{"right":"var:preset|spacing|xxx-small","left":"var:preset|spacing|xxx-small","top":"var:preset|spacing|xxx-small","bottom":"var:preset|spacing|xxx-small"}}},"backgroundColor":"primary-light","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-primary-light-background-color has-background" style="padding-top:var(--wp--preset--spacing--xxx-small);padding-right:var(--wp--preset--spacing--xxx-small);padding-bottom:var(--wp--preset--spacing--xxx-small);padding-left:var(--wp--preset--spacing--xxx-small)"><!-- wp:heading {"fontSize":"large"} -->
+<h2 class="wp-block-heading has-large-font-size">Find us</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Dolor Sit, 567 89</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|xxx-small","padding":{"top":"var:preset|spacing|xxx-small","bottom":"var:preset|spacing|xxx-small","left":"var:preset|spacing|xxx-small","right":"var:preset|spacing|xxx-small"}}},"backgroundColor":"primary-light","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-primary-light-background-color has-background" style="padding-top:var(--wp--preset--spacing--xxx-small);padding-right:var(--wp--preset--spacing--xxx-small);padding-bottom:var(--wp--preset--spacing--xxx-small);padding-left:var(--wp--preset--spacing--xxx-small)"><!-- wp:heading {"fontSize":"large"} -->
+<h2 class="wp-block-heading has-large-font-size">Follow us</h2>
+<!-- /wp:heading -->
+
+<!-- wp:social-links {"iconColor":"darko-transit","iconColorValue":"#171f29","iconBackgroundColor":"transparent","iconBackgroundColorValue":"transparent","className":"is-style-icecubo-social-outline","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|xxx-small"}}},"layout":{"type":"flex","justifyContent":"left"}} -->
+<ul class="wp-block-social-links has-icon-color has-icon-background-color is-style-icecubo-social-outline"><!-- wp:social-link {"url":"#","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"#","service":"linkedin"} /-->
+
+<!-- wp:social-link {"url":"#","service":"instagram"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+ <!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide"><!-- wp:heading -->
+<h2 class="wp-block-heading">Have a Question?</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>You should put a contact form beneath. First, you should install a plugin, for example "Contact form 7", then insert its shortcode. Also remove this paragraph.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:shortcode /--></div>
+<!-- /wp:group -->
