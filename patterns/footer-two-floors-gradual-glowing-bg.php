7aa2e15b9c674283662fe438ca218946c73a1789
Slobodan <sloooba@yahoo.com>
2025-07-21 23:41:30 +0200

    Update the patterns

--- patterns/footer-two-floors-gradual-glowing-bg.php
+++ patterns/footer-two-floors-gradual-glowing-bg.php
@@ -10,7 +10,14 @@
 <!-- wp:cover {"dimRatio":50,"gradient":"primary-to-transparent-rushly-radial","isDark":false,"metadata":{"categories":["icecubo-footer"],"patternName":"icecubo/footer-2floors-gradual-glowing-bg","name":"Two floors - Gradual glowing backgroud and horizontal lines at the top."},"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|large"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-cover alignwide is-light" style="padding-top:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--large)"><span aria-hidden="true" class="wp-block-cover__background has-background-dim has-background-gradient has-primary-to-transparent-rushly-radial-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"textColor":"handle-contrast-2"} -->
 <div class="wp-block-group has-handle-contrast-2-color has-text-color has-link-color">
-    <!-- wp:pattern {"slug":"icecubo/footer-two-floors-inner-2separators-top"} /-->
+    <!-- wp:separator {"className":"is-style-wide"} -->
+    <hr class="wp-block-separator is-style-wide"/>
+    <!-- /wp:separator -->
+
+    <!-- wp:separator {"className":"is-style-wide"} -->
+    <hr class="wp-block-separator is-style-wide"/>
+    <!-- /wp:separator -->
+    <!-- wp:pattern {"slug":"icecubo/footer-2floors-inner"} /-->
 </div>
 <!-- /wp:group --></div></div>
 <!-- /wp:cover -->
--- patterns/footer-two-floors.php
+++ patterns/footer-two-floors.php
@@ -1,7 +1,7 @@
 <?php
 // phpcs:ignore
 /**
- * Title: Two floors.
+ * Title: Two floors - Separated.
  * Slug: icecubo/footer-2floors
  * Categories: icecubo-footer
  * Block Types: core/template-part/footer
@@ -9,86 +9,8 @@
 ?>
 <!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|tint-3"}}},"spacing":{"padding":{"right":"0","left":"0"}}},"backgroundColor":"darko","textColor":"gray-light","layout":{"type":"constrained"},"fontSize":"small"} -->
 <div class="wp-block-group alignfull has-gray-light-color has-darko-background-color has-text-color has-background has-link-color has-small-font-size" style="padding-right:0;padding-left:0"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|small"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--small)"><!-- wp:columns {"verticalAlignment":"top","align":"wide"} -->
-<div class="wp-block-columns alignwide are-vertically-aligned-top"><!-- wp:column {"verticalAlignment":"top","width":"40%","layout":{"type":"constrained","justifyContent":"left"}} -->
-<div class="wp-block-column is-vertically-aligned-top" style="flex-basis:40%"><!-- wp:paragraph {"fontSize":"large"} -->
-<p class="has-large-font-size">About Us</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:column -->
-
-<!-- wp:column {"verticalAlignment":"top","width":"30%","layout":{"type":"constrained"}} -->
-<div class="wp-block-column is-vertically-aligned-top" style="flex-basis:30%"><!-- wp:paragraph {"fontSize":"large"} -->
-<p class="has-large-font-size">Services</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:list -->
-<ul><!-- wp:list-item -->
-<li><a href="#">Web design</a></li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li><a href="#">Web Development</a></li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li><a href="#">Copywriting</a></li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li><a href="#">Marketing</a></li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li><a href="#">Media</a></li>
-<!-- /wp:list-item --></ul>
-<!-- /wp:list --></div>
-<!-- /wp:column -->
-
-<!-- wp:column {"verticalAlignment":"top","width":"30%","layout":{"type":"constrained","justifyContent":"center"}} -->
-<div class="wp-block-column is-vertically-aligned-top" style="flex-basis:30%"><!-- wp:paragraph {"fontSize":"large"} -->
-<p class="has-large-font-size">Resources</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:list -->
-<ul><!-- wp:list-item -->
-<li><a href="#">Blog</a></li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li><a href="#">Knowledgebase</a></li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li><a href="#">Site audit</a></li>
-<!-- /wp:list-item --></ul>
-<!-- /wp:list --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns --></div>
-<!-- /wp:group -->
-
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","bottom":"var:preset|spacing|x-small"}},"border":{"top":{"color":"var:preset|color|gray-dark","width":"1px","style":"solid"}}},"className":"is-style-default","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull is-style-default" style="border-top-color:var(--wp--preset--color--gray-dark);border-top-style:solid;border-top-width:1px;padding-top:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-small)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-<div class="wp-block-group alignwide"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"left","orientation":"horizontal"}} -->
-<div class="wp-block-group"><!-- wp:paragraph -->
-<p>©</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:site-title {"level":0} /--></div>
-<!-- /wp:group -->
-
-<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","justifyContent":"space-between"},"style":{"spacing":{"blockGap":"var:preset|spacing|small"}}} /-->
-
-<!-- wp:social-links {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|small","left":"var:preset|spacing|small"}}},"className":"has-icon-color has-icon-background-color is-style-default"} -->
-<ul class="wp-block-social-links has-icon-color has-icon-background-color is-style-default"><!-- wp:social-link {"url":"#","service":"facebook"} /-->
-
-<!-- wp:social-link {"url":"#","service":"x"} /-->
-
-<!-- wp:social-link {"url":"#","service":"linkedin"} /--></ul>
-<!-- /wp:social-links --></div>
-<!-- /wp:group --></div>
+<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--small)">
+        <!-- wp:pattern {"slug":"icecubo/footer-two-floors-separated-inner"} /-->
+</div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
