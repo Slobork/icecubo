efb61eb8c5d8af95dc29b87b26b753e0f9521c1d
Slobodan <sloooba@yahoo.com>
2025-08-10 16:10:46 +0200

    Add the file

---
+++ patterns/testimonials-aside-img-quote-block-2.php
@@ -0,0 +1,49 @@
+<?php
+// phpcs:ignore
+/**
+ * Title: Testimonials in columns with image on one side, stars and quote block on the other.
+ * Slug: icecubo/testimonials-aside-image-quote-block-2
+ * Categories: icecubo-testimonials
+ */
+?>
+<!-- wp:columns {"align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|large"}}}} -->
+<div class="wp-block-columns alignwide" style="margin-top:var(--wp--preset--spacing--large)"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+<div class="wp-block-group"><!-- wp:image {"scale":"cover","sizeSlug":"full","linkDestination":"none","className":"is-style-rounded"} -->
+<figure class="wp-block-image size-full is-style-rounded"><img alt="" style="object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"left","style":{"elements":{"link":{"color":{"text":"#ffce3a"}}},"color":{"text":"#ffce3a"},"layout":{"selfStretch":"fit","flexSize":null}}} -->
+<p class="has-text-align-left has-text-color has-link-color" style="color:#ffce3a">★★★★★</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:quote {"textAlign":"right","className":"is-style-plain","style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"blockGap":"var:preset|spacing|x-small"}}} -->
+<blockquote class="wp-block-quote has-text-align-right is-style-plain"><!-- wp:paragraph {"align":"left"} -->
+<p class="has-text-align-left">Cursus in hac habitasse platea dictumst quisque. Senectus et netus et malesuada fames ac turpis.</p>
+<!-- /wp:paragraph --><cite>— John Doe, ABC Inc.</cite></blockquote>
+<!-- /wp:quote --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+<div class="wp-block-group"><!-- wp:image {"scale":"cover","sizeSlug":"full","linkDestination":"none","className":"is-style-rounded"} -->
+<figure class="wp-block-image size-full is-style-rounded"><img alt="" style="object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"left","style":{"elements":{"link":{"color":{"text":"#ffce3a"}}},"color":{"text":"#ffce3a"},"layout":{"selfStretch":"fit","flexSize":null}}} -->
+<p class="has-text-align-left has-text-color has-link-color" style="color:#ffce3a">★★★★★</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:quote {"textAlign":"right","className":"is-style-plain","style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"blockGap":"var:preset|spacing|x-small"}}} -->
+<blockquote class="wp-block-quote has-text-align-right is-style-plain"><!-- wp:paragraph {"align":"left"} -->
+<p class="has-text-align-left">Cursus in hac habitasse platea dictumst quisque. Senectus et netus et malesuada fames ac turpis.</p>
+<!-- /wp:paragraph --><cite>— Jane Doe, XYZ Inc.</cite></blockquote>
+<!-- /wp:quote --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
