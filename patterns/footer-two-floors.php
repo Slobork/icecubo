425dae8bdce201e6db10bb380a67ed8000a95e92
Slobodan <sloooba@yahoo.com>
2025-12-27 16:58:23 +0100

    Add metadata and footer tag

--- patterns/footer-two-floors.php
+++ patterns/footer-two-floors.php
@@ -7,10 +7,10 @@
  * Block Types: core/template-part/footer
  */
 ?>
-<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|tint-3"}}},"spacing":{"padding":{"right":"0","left":"0"}}},"backgroundColor":"darko","textColor":"gray-light","layout":{"type":"constrained"},"fontSize":"small"} -->
-<div class="wp-block-group alignfull has-gray-light-color has-darko-background-color has-text-color has-background has-link-color has-small-font-size" style="padding-right:0;padding-left:0"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|small"}}},"layout":{"type":"constrained"}} -->
+<!-- wp:group {"metadata":{"name":"Footer"},"tagName":"footer","align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|tint-3"}}},"spacing":{"padding":{"right":"0","left":"0"}}},"backgroundColor":"darko","textColor":"gray-light","layout":{"type":"constrained"},"fontSize":"small"} -->
+<footer class="wp-block-group alignfull has-gray-light-color has-darko-background-color has-text-color has-background has-link-color has-small-font-size" style="padding-right:0;padding-left:0"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|small"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--small)">
         <!-- wp:pattern {"slug":"icecubo/footer-two-floors-separated-inner"} /-->
 </div>
-<!-- /wp:group --></div>
+<!-- /wp:group --></footer>
 <!-- /wp:group -->
