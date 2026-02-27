6b0c6f6ae552dbe36d5794b2a847fe8ee6f7ea07
Slobodan <sloooba@yahoo.com>
2026-02-27 18:14:54 +0100

    Update the settings

--- inc/settings-page.php
+++ inc/settings-page.php
@@ -57,7 +57,7 @@ if ( ! function_exists( 'icecubo_theme_options' ) ) {
             esc_html__('IceCubo Theme', 'icecubo'),
             'manage_options',
             'icecubo-theme-options',
-            'icecubo_theme_options_page'
+            'icecubo_theme_options_page_content'
         );
 
         add_action('admin_init', 'icecubo_register_settings');
@@ -69,18 +69,18 @@ if ( ! function_exists( 'icecubo_theme_options' ) ) {
 
 
 function icecubo_register_settings() {
-    // First section settings
+    // First section settings - empty title for now, just to display the info box
     add_settings_section(
         'section_one',
-        'Section One Settings',
+        '',
         'section_one_callback',
         'icecubo-theme-options'
     );
-    // Info box with background image
+    // Info box field
     add_settings_field(
-        'info_background',
-        'Information Boxes',
-        'icecubo_info_background_callback',
+        'info_boxes',
+        esc_html__('Get started with IceCubo', 'icecubo'),
+        'icecubo_info_boxes_callback',
         'icecubo-theme-options',
         'section_one'
     );
@@ -88,11 +88,12 @@ function icecubo_register_settings() {
 
 
 function section_one_callback() {
-    echo '<p>Settings for the first section.</p>';
+    // empty for now, we don't need any text here since the info box has its own title and description. But we need this callback to register the section and display the info box field.
+    echo '';
 }
 
-
-function icecubo_theme_options_page() {
+// Content of the theme options page
+function icecubo_theme_options_page_content() {
     ?>
     <div class="wrap">
         <h1>IceCubo Theme Options</h1>
@@ -106,11 +107,17 @@ function icecubo_theme_options_page() {
     <?php
 }
 
+// Get license key from the database
+function icecubo_get_license() {
+    $licenseKey = get_option("IceCubo_Pro-lic_Key", "");
+    return $licenseKey;
+}
+
 /**
  * Render an info box with a background image.
- * Declared here so the callback is available when settings are registered.
+ * The callback is available when settings are registered.
  */
-function icecubo_info_background_callback() {
+function icecubo_info_boxes_callback() {
     $img = get_theme_file_uri(). "/assets/img/ice-cubes.png";
     echo '<div id="ice-settings" style="color: white; display: flex; flex-wrap: wrap; justify-content: space-around; gap: 30px; background-image: url(' .esc_url($img) . '); width: fit-content; background-size: contain; background-position: center; padding: 60px 30px; border-radius:50%; margin-bottom:10px;">';
     
@@ -124,17 +131,22 @@ function icecubo_info_background_callback() {
     echo '<p style="line-height: 1.75">' . esc_html__( 'Start editing the front page and access other templates from the Editor.', 'icecubo' ) . '</p>';
     echo '<a style="font-size: 16px; line-height: 1.7; color: #a3a3ff;" href="site-editor.php">Go to the Editor →</a>';
     echo '</div>';
-    if (! class_exists('Ice_Cubo_Pro') ) {
+    if (! class_exists('IceCubo_Pro') ) {
         echo '<div style="background: rgb(6 9 34 / 88%); color: white; padding: 20px; border-radius:4px; max-width:400px; border: 10px solid #a3a3ff;">';
         echo '<h3 style="margin:0 0 10px; color: white;">' . esc_html__( 'Buy Pro', 'icecubo' ) . '</h3>';
         echo '<p style="line-height: 1.75">' . esc_html__( 'With Pro version get advanced features and templates.', 'icecubo' ) . '</p>';
-        echo '<a style="font-size: 16px; line-height: 1.7; color: #a3a3ff;" href="https://maxpressy.com" target="_blank">Get Pro Version →</a>';
+        echo '<a style="font-size: 16px; line-height: 1.7; color: #a3a3ff;" href="https://maxpressy.com/icecubo/" target="_blank">Get Pro Version →</a>';
         echo '</div>';
     } else {
         echo '<div style="background: rgb(6 9 34 / 88%); color: white; padding: 20px; border-radius:4px; max-width:400px;">';
         echo '<h3 style="margin:0 0 10px; color: white;">' . esc_html__( 'Install And Activate Icecubo Plugins', 'icecubo' ) . '</h3>';
-        echo '<p style="line-height: 1.75">' . esc_html__( 'Enable additional addons and templates. Once installed, Icecubo plugins will be available from the regular WP Plugins page.', 'icecubo' ) . '</p>';
-        echo '<a style="font-size: 16px; line-height: 1.7; color: #a3a3ff;" href="plugins.php?page=icecubo-install-plugins&plugin_status=activate">Go to the Ice plugins →</a>';
+        if(icecubo_get_license() !='') {
+            echo '<p style="line-height: 1.75">' . esc_html__( 'Enable additional addons and templates. Once installed, Icecubo plugins will be available from the regular WP Plugins page.', 'icecubo' ) . '</p>';
+            echo '<a style="font-size: 16px; line-height: 1.7; color: #a3a3ff;" href="plugins.php?page=icecubo-install-plugins&plugin_status=activate">Go to the Ice plugins →</a>';
+        } else {
+            echo '<a style="font-size: 16px; line-height: 2.5; background: #a3a3ff; color: black; padding: 5px; border-radius: 3px; margin: 0 30px 0 10px;" href="admin.php?page=icecubo-licenses">Activate License first to access aaddons →</a>';
+        }
+        
         echo '</div>';
     }
 
