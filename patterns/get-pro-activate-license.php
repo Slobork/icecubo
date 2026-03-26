2c4a3cfb15cfe13f6d4e60c71bf5312ded099989
Slobodan <sloooba@yahoo.com>
2026-03-26 12:14:30 +0100

    Add the file

---
+++ patterns/get-pro-activate-license.php
@@ -0,0 +1,21 @@
+<?php
+// phpcs:ignore
+if (! defined('ABSPATH') ) {
+    exit;
+}
+/**
+ * Title: Activate IceCubo Pro license.
+ * Slug: icecubo/get-pro-activate-license
+ * Categories: hidden
+ * Inserter: false
+ */
+?>
+<!-- wp:cover {"customOverlayColor":"#1c1c2f","isUserOverlayColor":true,"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim" style="background-color:#1c1c2f"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"placeholder":"Write title…","style":{"typography":{"textAlign":"center"},"elements":{"link":{"color":{"text":"#006fff"}}}},"textColor":"white-ice","fontSize":"large"} -->
+<p class="has-text-align-center has-white-ice-color has-text-color has-link-color has-large-font-size">Great you have activated the IceCubo Pro plugin, but still you have to activate its license to get more patterns!</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph {"placeholder":"Write title…","style":{"typography":{"textAlign":"center"},"elements":{"link":{"color":{"text":"#006fff"}}}},"textColor":"white-ice","fontSize":"large"} -->
+<p class="has-text-align-center has-white-ice-color has-text-color has-link-color has-large-font-size">From the Admin dashboard go to the "Appearance" → "IceCubo License" page to activate your license.</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover -->
+ 
