fed07a5ba2d340842bba9401630d0a25d76bd9da
Slobodan <sloooba@yahoo.com>
2026-03-27 11:58:22 +0100

    Change the title

--- patterns/header-basic-wide-cover-gradient-light.php
+++ patterns/header-basic-wide-cover-gradient-light.php
@@ -4,7 +4,7 @@ if (! defined('ABSPATH') ) {
     exit;
 }
 /**
- * Title: Header: Spread elements: Logo - nav. Glowing background transit to transparent.
+ * Title: Header: Spread elements: Logo - nav. Light glowing background transit to transparent.
  * Slug: icecubo/header-basic-wide-cover-gradient-light
  * Categories: icecubo-header
  * Block Types: core/template-part/header
