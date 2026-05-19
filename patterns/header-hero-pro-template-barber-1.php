516635c0d85fe8ec18bb05dc408d983f81a80f05
Slobodan <sloooba@yahoo.com>
2026-05-19 17:21:22 +0200

    Add 'barber' headers

---
+++ patterns/header-hero-pro-template-barber-1.php
@@ -0,0 +1,26 @@
+<?php
+// phpcs:ignore
+if (! defined('ABSPATH') ) {
+    exit;
+}
+/**
+ * Title: Template Barber (1): Header With A Hero Section (Premium).
+ * Slug: icecubo/header-hero-pro-template-barber-1
+ * Categories: icecubo-headerhero
+ * Block Types: core/template-part/header
+ */
+if(!class_exists('IceCubo_Pro')) {
+?>
+    <!-- wp:pattern {"slug":"icecubo/get-pro-patterns"} /-->
+<?php
+} else {
+    if(IceCubo_Pro::getLicense() === '') {
+    ?>
+        <!-- wp:pattern {"slug":"icecubo/get-pro-activate-license"} /-->
+    <?php
+    } else {
+    ?>
+        <!-- wp:pattern {"slug":"icecubo/prem-header-template-barber-1"} /-->
+    <?php
+    }
+}
