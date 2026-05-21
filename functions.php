f23cdf8fa579ef1c95ab6f4c22041a664ba73cff
Slobodan <sloooba@yahoo.com>
2026-05-21 14:59:56 +0200

    Bump a version

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ if (! defined('ABSPATH') ) {
  * Author URI:          https://maxpressy.com
  * License:             GPL v2 or later
  * License URI:         https://www.gnu.org/licenses/gpl-2.0.html
- * Version:             1.2.4
+ * Version:             1.2.5
  * Text Domain:         icecubo
  * Domain Path:         /languages
  * Requires at least:   6.1
