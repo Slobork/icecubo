10ab1f2a4afd9492cd0cbe999e2db1251da82cf3
Slobodan <sloooba@yahoo.com>
2026-07-17 19:46:15 +0200

    Add the file

---
+++ patterns/navigation-overlay-5.php
@@ -0,0 +1,19 @@
+<?php
+// phpcs:ignore
+if (! defined('ABSPATH') ) {
+    exit;
+}
+/**
+ * Title: Dark with Rotating Animation. Aligned Left.
+ * Slug: icecubo/navigation-overlay-5
+ * Categories: icecubo-navigation-overlay
+ * Block Types: core/template-part/navigation-overlay
+ */
+?>
+<!-- wp:group {"metadata":{"patternName":"icecubo/navigation-overlay-5","name":"Dark with Rotating Animation. Aligned Left.","categories":["icecubo-navigation-overlay"]},"className":"ice-anim anim-rotaterev-right","style":{"border":{"radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"50px","bottomRight":"50px"}},"spacing":{"padding":{"top":"var:preset|spacing|0","bottom":"var:preset|spacing|0","left":"var:preset|spacing|0","right":"var:preset|spacing|0"}}},"backgroundColor":"classy-3","layout":{"type":"flex","orientation":"vertical","justifyContent":"left","flexWrap":"wrap"}} -->
+<div class="wp-block-group ice-anim anim-rotaterev-right has-classy-3-background-color has-background" style="border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:50px;border-bottom-right-radius:50px;padding-top:var(--wp--preset--spacing--0);padding-right:var(--wp--preset--spacing--0);padding-bottom:var(--wp--preset--spacing--0);padding-left:var(--wp--preset--spacing--0)"><!-- wp:cover {"dimRatio":90,"isUserOverlayColor":true,"gradient":"darko-to-primary-linear-gradual","contentPosition":"center center","style":{"spacing":{"margin":{"top":"var:preset|spacing|0","bottom":"var:preset|spacing|0"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"css":"width:100%"},"textColor":"white","layout":{"type":"default"}} -->
+<div class="wp-block-cover has-custom-css has-white-color has-text-color has-link-color" style="margin-top:var(--wp--preset--spacing--0);margin-bottom:var(--wp--preset--spacing--0)"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-90 has-background-dim has-background-gradient has-darko-to-primary-linear-gradual-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:navigation-overlay-close {"className":"ice-anim anim-scale-in-hor-center is-style-icecubo-navigation-overlay-close-circled","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} /-->
+
+<!-- wp:navigation {"showSubmenuIcon":false,"submenuVisibility":"always","overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"}} /--></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->
---
+++ patterns/navigation-overlay-6.php
@@ -0,0 +1,19 @@
+<?php
+// phpcs:ignore
+if (! defined('ABSPATH') ) {
+    exit;
+}
+/**
+ * Title: Light with Fade Up Animation. Aligned Left.
+ * Slug: icecubo/navigation-overlay-6
+ * Categories: icecubo-navigation-overlay
+ * Block Types: core/template-part/navigation-overlay
+ */
+?>
+<!-- wp:group {"metadata":{"patternName":"icecubo/navigation-overlay-6","name":"Light with Fade Up Animation. Aligned Left.","categories":["icecubo-navigation-overlay"]},"style":{"border":{"radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"50px","bottomRight":"50px"}},"spacing":{"padding":{"top":"var:preset|spacing|0","bottom":"var:preset|spacing|0","left":"var:preset|spacing|0","right":"var:preset|spacing|0"}}},"backgroundColor":"white-ice","layout":{"type":"flex","orientation":"vertical","justifyContent":"left","flexWrap":"wrap"}} -->
+<div class="wp-block-group has-white-ice-background-color has-background" style="border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:50px;border-bottom-right-radius:50px;padding-top:var(--wp--preset--spacing--0);padding-right:var(--wp--preset--spacing--0);padding-bottom:var(--wp--preset--spacing--0);padding-left:var(--wp--preset--spacing--0)"><!-- wp:cover {"dimRatio":30,"isUserOverlayColor":true,"gradient":"matching-colors-diagonal","isDark":false,"style":{"spacing":{"margin":{"top":"var:preset|spacing|0","bottom":"var:preset|spacing|0"}},"css":"width:100%"},"layout":{"type":"default"}} -->
+<div class="wp-block-cover is-light has-custom-css" style="margin-top:var(--wp--preset--spacing--0);margin-bottom:var(--wp--preset--spacing--0)"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-30 has-background-dim has-background-gradient has-matching-colors-diagonal-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:navigation-overlay-close {"className":"ice-anim anim-scale-in-ver-center is-style-icecubo-navigation-overlay-close-circled"} /-->
+
+<!-- wp:navigation {"showSubmenuIcon":false,"submenuVisibility":"always","overlayMenu":"never","className":"ice-anim anim-fadeInUp","layout":{"type":"flex","orientation":"vertical","justifyContent":"left"}} /--></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->
