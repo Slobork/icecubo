d73e3fc6da87488a69ee19b2d75cf1a5111ff497
Slobodan <sloooba@yahoo.com>
2026-07-20 14:23:30 +0200

    Add the file

---
+++ patterns/navigation-overlay-11.php
@@ -0,0 +1,21 @@
+<?php
+// phpcs:ignore
+if (! defined('ABSPATH') ) {
+    exit;
+}
+/**
+ * Title: Mix Light Colors with Scaling Animation from the Top Right. Large Curved Left Edge. Aligned right.
+ * Slug: icecubo/navigation-overlay-11
+ * Categories: icecubo-navigation-overlay
+ * Block Types: core/template-part/navigation-overlay
+ */
+?>
+<!-- wp:group {"metadata":{"patternName":"icecubo/navigation-overlay-11","name":"Mix Light Colors with Scaling Animation from the Top Right. Large Curved Left Edge. Aligned right.","categories":["icecubo-navigation-overlay"]},"className":"ice-anim anim-scale-in-tr del025","style":{"border":{"radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"50%","bottomRight":"50px"}},"spacing":{"padding":{"top":"var:preset|spacing|0","bottom":"var:preset|spacing|0","left":"var:preset|spacing|0","right":"var:preset|spacing|0"}}},"backgroundColor":"primary-light","layout":{"type":"flex","orientation":"vertical","flexWrap":"wrap"}} -->
+<div class="wp-block-group ice-anim anim-scale-in-tr del025 has-primary-light-background-color has-background" style="border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:50%;border-bottom-right-radius:50px;padding-top:var(--wp--preset--spacing--0);padding-right:var(--wp--preset--spacing--0);padding-bottom:var(--wp--preset--spacing--0);padding-left:var(--wp--preset--spacing--0)"><!-- wp:cover {"dimRatio":50,"isUserOverlayColor":true,"gradient":"radial-transparent-to-classy","contentPosition":"top right","isDark":false,"style":{"css":"width:100%; border-radius: inherit;"},"layout":{"type":"default"}} -->
+<div class="wp-block-cover is-light has-custom-content-position is-position-top-right has-custom-css"><span aria-hidden="true" class="wp-block-cover__background has-background-dim has-background-gradient has-radial-transparent-to-classy-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"right"}} -->
+<div class="wp-block-group"><!-- wp:navigation-overlay-close {"className":"ice-anim anim-scale-in-ver-center is-style-icecubo-navigation-overlay-close-circled"} /-->
+
+<!-- wp:navigation {"ref":33518,"showSubmenuIcon":false,"submenuVisibility":"always","overlayMenu":"never","layout":{"type":"flex","orientation":"vertical","justifyContent":"right"}} /--></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->
---
+++ patterns/navigation-overlay-12.php
@@ -0,0 +1,21 @@
+<?php
+// phpcs:ignore
+if (! defined('ABSPATH') ) {
+    exit;
+}
+/**
+ * Title: Mix Matching and Primary Colors with Horizontal Scaling Animation. Large Curved Left Edge. Aligned left.
+ * Slug: icecubo/navigation-overlay-12
+ * Categories: icecubo-navigation-overlay
+ * Block Types: core/template-part/navigation-overlay
+ */
+?>
+<!-- wp:group {"metadata":{"patternName":"icecubo/navigation-overlay-12","name":"Mix Matching and Primary Colors with Horizontal Scaling Animation. Large Curved Left Edge. Aligned left.","categories":["icecubo-navigation-overlay"]},"className":"ice-anim anim-scale-in-hor-center del025","style":{"border":{"radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"50%","bottomRight":"0px"}},"spacing":{"padding":{"top":"var:preset|spacing|0","bottom":"var:preset|spacing|0","left":"var:preset|spacing|0","right":"var:preset|spacing|0"}}},"backgroundColor":"matching-4","layout":{"type":"flex","orientation":"vertical","flexWrap":"wrap"}} -->
+<div class="wp-block-group ice-anim anim-scale-in-hor-center del025 has-matching-4-background-color has-background" style="border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:50%;border-bottom-right-radius:0px;padding-top:var(--wp--preset--spacing--0);padding-right:var(--wp--preset--spacing--0);padding-bottom:var(--wp--preset--spacing--0);padding-left:var(--wp--preset--spacing--0)"><!-- wp:cover {"dimRatio":60,"isUserOverlayColor":true,"gradient":"primary-to-transparent-gradual-radial","contentPosition":"top right","style":{"css":"width:100%; border-radius: inherit;"},"layout":{"type":"default"}} -->
+<div class="wp-block-cover has-custom-content-position is-position-top-right has-custom-css"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-60 has-background-dim has-background-gradient has-primary-to-transparent-gradual-radial-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"right"}} -->
+<div class="wp-block-group"><!-- wp:navigation-overlay-close {"className":"ice-anim anim-scale-in-ver-center is-style-icecubo-navigation-overlay-close-circled","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} /-->
+
+<!-- wp:navigation {"showSubmenuIcon":false,"submenuVisibility":"always","overlayMenu":"never","layout":{"type":"flex","orientation":"vertical","justifyContent":"right"}} /--></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->
