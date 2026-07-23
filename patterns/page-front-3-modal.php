835dd8e91eec770ceeed62201a0f369f5b761078
Slobodan <sloooba@yahoo.com>
2026-07-23 15:03:38 +0200

    Replace the Header group with a Header Template part

--- patterns/page-front-3-modal.php
+++ patterns/page-front-3-modal.php
@@ -11,12 +11,7 @@ if (! defined('ABSPATH') ) {
  * Template Types: front-page
  */
 ?>
-<!-- wp:group {"metadata":{"name":"Header"},"tagName":"header","align":"full","layout":{"type":"default"}} -->
-<header class="wp-block-group alignfull">
-    <!-- wp:pattern {"slug":"icecubo/header-hero-cover-two-columns-cta-above-wide-img"} /-->
-</header>
-<!-- /wp:group -->
-
+<!-- wp:template-part {"slug":"header-hero-cover-two-columns-cta-above-wide-img","tagName":"header"} /-->
 <!-- wp:group {"metadata":{"name":"Main"},"tagName":"main","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|x-large"}}},"layout":{"type":"constrained"}} -->
 <main class="wp-block-group" style="padding-bottom:var(--wp--preset--spacing--x-large)">
     <!-- wp:pattern {"slug":"icecubo/page-front-3-main"} /-->
