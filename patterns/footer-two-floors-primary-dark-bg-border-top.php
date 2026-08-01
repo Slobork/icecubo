6fa253e1669d1a2344970d2b3f16a021955cc3ec
Slobodan <sloooba@yahoo.com>
2026-08-01 14:04:25 +0200

    Add the file

---
+++ patterns/footer-two-floors-primary-dark-bg-border-top.php
@@ -0,0 +1,19 @@
+<?php
+// phpcs:ignore
+if (! defined('ABSPATH') ) {
+    exit;
+}
+/**
+ * Title: Footer: Two floors - Dark primary background with a border at the top.
+ * Slug: icecubo/footer-2floors-primary-dark-bg-border-top
+ * Categories: icecubo-footer
+ * Block Types: core/template-part/footer
+ */
+?>
+<!-- wp:cover {"overlayColor":"primary-dark","isUserOverlayColor":true,"minHeightUnit":"px","align":"full","style":{"border":{"top":{"color":"var:preset|color|primary","width":"10px"},"right":{},"bottom":{},"left":{}}},"layout":{"type":"default"}} -->
+<div class="wp-block-cover alignfull" style="border-top-color:var(--wp--preset--color--primary);border-top-width:10px"><span aria-hidden="true" class="wp-block-cover__background has-primary-dark-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"0","left":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|handle-contrast-5"}}}},"textColor":"handle-contrast-3","fontSize":"small","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-handle-contrast-3-color has-text-color has-link-color has-small-font-size" style="padding-right:0;padding-left:0">
+    <!-- wp:pattern {"slug":"icecubo/footer-2floors-inner"} /-->
+</div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
