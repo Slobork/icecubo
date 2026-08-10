5a97c9076c5687857ee14e1a234d0b96e312cee2
Slobodan <sloooba@yahoo.com>
2026-08-10 17:48:10 +0200

    Spacing and styling adjustment

--- patterns/navigation-overlay-3.php
+++ patterns/navigation-overlay-3.php
@@ -4,14 +4,14 @@ if (! defined('ABSPATH') ) {
     exit;
 }
 /**
- * Title: Gradient Shading Overlay With Left Alignment.
+ * Title: Gradient Shading With Left Alignment.
  * Slug: icecubo/navigation-overlay-3
  * Categories: icecubo-navigation-overlay
  * Block Types: core/template-part/navigation-overlay
  */
 ?>
-<!-- wp:group {"metadata":{"patternName":"icecubo/navigation-overlay-3","name":"Gradient Shading Overlay With Left Alignment.","categories":["icecubo-navigation-overlay"]},"className":"ice-anim anim-slide-bg","gradient":"shade-2-to-primaryish-to-shade-4-diagonal","layout":{"type":"flex","orientation":"vertical"}} -->
-<div class="wp-block-group ice-anim anim-slide-bg has-shade-2-to-primaryish-to-shade-4-diagonal-gradient-background has-background"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<!-- wp:group {"metadata":{"patternName":"icecubo/navigation-overlay-3","name":"Gradient Shading With Left Alignment.","categories":["icecubo-navigation-overlay"]},"className":"ice-anim anim-slide-bg","style":{"border":{"radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"0px","bottomRight":"10%"}},"shadow":"var:preset|shadow|crisp-current"},"gradient":"shade-2-to-primaryish-to-shade-4-diagonal","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group ice-anim anim-slide-bg has-shade-2-to-primaryish-to-shade-4-diagonal-gradient-background has-background" style="border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:10%;box-shadow:var(--wp--preset--shadow--crisp-current)"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
 <div class="wp-block-group"><!-- wp:navigation-overlay-close {"className":"is-style-icecubo-navigation-overlay-close-circled"} /-->
 
 <!-- wp:navigation {"showSubmenuIcon":false,"submenuVisibility":"always","overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"}} /--></div>
