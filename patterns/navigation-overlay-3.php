f156e89817b474c7d5df893832b18fa08144877d
Slobodan <sloooba@yahoo.com>
2026-08-11 15:43:25 +0200

    Adjust the styling

--- patterns/navigation-overlay-3.php
+++ patterns/navigation-overlay-3.php
@@ -10,8 +10,8 @@ if (! defined('ABSPATH') ) {
  * Block Types: core/template-part/navigation-overlay
  */
 ?>
-<!-- wp:group {"metadata":{"patternName":"icecubo/navigation-overlay-3","name":"Gradient Shading With Left Alignment.","categories":["icecubo-navigation-overlay"]},"className":"ice-anim anim-slide-bg","style":{"border":{"radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"0px","bottomRight":"10%"}},"shadow":"var:preset|shadow|crisp-current"},"gradient":"shade-2-to-primaryish-to-shade-4-diagonal","layout":{"type":"flex","orientation":"vertical"}} -->
-<div class="wp-block-group ice-anim anim-slide-bg has-shade-2-to-primaryish-to-shade-4-diagonal-gradient-background has-background" style="border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:10%;box-shadow:var(--wp--preset--shadow--crisp-current)"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<!-- wp:group {"metadata":{"patternName":"icecubo/navigation-overlay-3","name":"Gradient Shading With Left Alignment.","categories":["icecubo-navigation-overlay"]},"className":"ice-anim anim-slide-bg","style":{"border":{"radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"0px","bottomRight":"12%"}},"shadow":"var:preset|shadow|crisp-current","spacing":{"margin":{"bottom":"var:preset|spacing|xx-small"}}},"gradient":"shade-2-to-primaryish-to-shade-4-diagonal","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group ice-anim anim-slide-bg has-shade-2-to-primaryish-to-shade-4-diagonal-gradient-background has-background" style="border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:12%;margin-bottom:var(--wp--preset--spacing--xx-small);box-shadow:var(--wp--preset--shadow--crisp-current)"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
 <div class="wp-block-group"><!-- wp:navigation-overlay-close {"className":"is-style-icecubo-navigation-overlay-close-circled"} /-->
 
 <!-- wp:navigation {"showSubmenuIcon":false,"submenuVisibility":"always","overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"}} /--></div>
