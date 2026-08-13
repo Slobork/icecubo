aac5c1c4dc02fbfd10aede69b84b5194b1a35872
Slobodan <sloooba@yahoo.com>
2026-08-13 15:25:58 +0200

    Add the file

---
+++ patterns/navigation-overlay-16.php
@@ -0,0 +1,19 @@
+<?php
+// phpcs:ignore
+if (! defined('ABSPATH') ) {
+    exit;
+}
+/**
+ * Title: "Transit Matching Background. Curved Edge at the Bottom Right.Menu Aligned Center with Close Button at Right."
+ * Slug: icecubo/navigation-overlay-16
+ * Categories: icecubo-navigation-overlay
+ * Block Types: core/template-part/navigation-overlay
+ */
+?>
+<!-- wp:group {"metadata":{"patternName":"icecubo/navigation-overlay-16","name":"Transit Matching With Left Alignment.","categories":["icecubo-navigation-overlay"]},"className":"ice-anim anim-slide-bg","style":{"border":{"radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"0px","bottomRight":"12%"}},"shadow":"var:preset|shadow|crisp-current","spacing":{"margin":{"bottom":"var:preset|spacing|xx-small"}}},"gradient":"matching-1-to-3-to-5","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group ice-anim anim-slide-bg has-matching-1-to-3-to-5-gradient-background has-background" style="border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:12%;margin-bottom:var(--wp--preset--spacing--xx-small);box-shadow:var(--wp--preset--shadow--crisp-current)"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"right"}} -->
+<div class="wp-block-group"><!-- wp:navigation-overlay-close {"className":"is-style-icecubo-navigation-overlay-close-circled"} /-->
+
+<!-- wp:navigation {"showSubmenuIcon":false,"submenuVisibility":"always","overlayMenu":"never","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
