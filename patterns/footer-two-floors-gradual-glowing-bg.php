b6ada67468fca3339d983ef8c077b43cf4e88091
Slobodan <sloooba@yahoo.com>
2026-09-01 11:17:10 +0200

    Update separator

--- patterns/footer-two-floors-gradual-glowing-bg.php
+++ patterns/footer-two-floors-gradual-glowing-bg.php
@@ -13,12 +13,11 @@ if (! defined('ABSPATH') ) {
 <!-- wp:cover {"dimRatio":50,"gradient":"primary-to-transparent-rushly-radial","isDark":false,"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|large"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-cover alignwide is-light" style="padding-top:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--large)"><span aria-hidden="true" class="wp-block-cover__background has-background-dim has-background-gradient has-primary-to-transparent-rushly-radial-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"textColor":"handle-contrast-2"} -->
 <div class="wp-block-group has-handle-contrast-2-color has-text-color has-link-color">
-    <!-- wp:separator {"className":"is-style-wide"} -->
-    <hr class="wp-block-separator is-style-wide"/>
+    <!-- wp:separator {"opacity":"css","className":"is-style-wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|xxx-small"}}},"gradient":"classy-to-transparent-radial-66-break"} -->
+    <hr class="wp-block-separator has-css-opacity has-classy-to-transparent-radial-66-break-gradient-background has-background is-style-wide" style="margin-top:var(--wp--preset--spacing--xxx-small)"/>
     <!-- /wp:separator -->
-
-    <!-- wp:separator {"className":"is-style-wide"} -->
-    <hr class="wp-block-separator is-style-wide"/>
+    <!-- wp:separator {"opacity":"css","className":"is-style-wide","gradient":"classy-to-transparent-radial-66-break"} -->
+    <hr class="wp-block-separator has-css-opacity has-classy-to-transparent-radial-66-break-gradient-background has-background is-style-wide"/>
     <!-- /wp:separator -->
     <!-- wp:pattern {"slug":"icecubo/footer-2floors-inner"} /-->
 </div>
