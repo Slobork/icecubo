b03d3c5756651a8e2aef7b71a993d682b5c829c3
Slobodan <sloooba@yahoo.com>
2026-09-09 20:22:41 +0200

    Add the breadcrumbs

--- patterns/page-modal-blog-featured-img-cover.php
+++ patterns/page-modal-blog-featured-img-cover.php
@@ -16,7 +16,9 @@ if (! defined('ABSPATH') ) {
 <!-- wp:group {"tagName":"main","style":{"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|x-large"},"margin":{"top":"0"}}},"layout":{"type":"constrained"}} -->
 <main class="wp-block-group" style="margin-top:0;padding-top:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--x-large)">
     <!-- wp:group {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|large"}}}} -->
-    <div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--large)"><!-- wp:heading {"level":1,"style":{"spacing":{"margin":{"top":"0"}}}} -->
+    <div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--large)">
+    <!-- wp:pattern {"slug":"icecubo/breadcrumbs"} /-->
+    <!-- wp:heading {"level":1,"style":{"spacing":{"margin":{"top":"0"}}}} -->
     <h1 class="wp-block-heading" style="margin-top:0"><?php echo esc_html__('The latest posts', 'icecubo'); ?></h1>
     <!-- /wp:heading --></div>
     <!-- /wp:group -->
