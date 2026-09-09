fef2526fe4a65ebea172cb44ac75c387db3e88ab
Slobodan <sloooba@yahoo.com>
2026-09-09 20:07:36 +0200

    Adjust the spacing

--- patterns/page-modal-blog-with-sidebar.php
+++ patterns/page-modal-blog-with-sidebar.php
@@ -13,8 +13,8 @@ if (! defined('ABSPATH') ) {
 ?>
 <!-- wp:template-part {"slug":"header","tagName":"header"} /-->
 
-<!-- wp:group {"tagName":"main","layout":{"type":"constrained"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","bottom":"var:preset|spacing|x-large"},"margin":{"top":"0"}}}} -->
-<main class="wp-block-group" style="margin-top:0;padding-top:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-large)">
+<!-- wp:group {"tagName":"main","layout":{"type":"constrained"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|x-large"},"margin":{"top":"0"}}}} -->
+<main class="wp-block-group" style="margin-top:0;padding-top:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--x-large)">
     <!-- wp:group {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|x-large"}}}} -->
     <div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--x-large)"><!-- wp:heading {"level":1,"style":{"spacing":{"margin":{"top":"0"}}}} -->
     <h1 class="wp-block-heading" style="margin-top:0"><?php echo esc_html__('The latest posts', 'icecubo'); ?></h1>
